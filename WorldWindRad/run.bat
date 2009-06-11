d11da223ff049ffe0d6354b2ca0d73182d5680bb
Michael de Hoog <michael.dehoog@ga.gov.au>
2009-06-11 01:32:14 +0000

    Removed unused sun calculator, fixed up some stuff in run.bat
    git-svn-id: svn://localhost/Software/Java/WorldWind@545 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- WorldWindRad/run.bat
+++ WorldWindRad/run.bat
@@ -1,1 +1,1 @@
-java -cp bin;lib/worldwind.jar;lib/jogl.jar;lib/gluegen-rt.jar -Djava.library.path=lib/jogl/windows-i586 -Dsun.java2d.noddraw-true au.gov.ga.worldwind.application.Application
+java -Xmx1024m -cp bin;lib/worldwind.jar;lib/jogl.jar;lib/gluegen-rt.jar -Djava.library.path=lib/jogl/windows-i586 -Dsun.java2d.noddraw=true au.gov.ga.worldwind.application.Application
