bda4c495c15889ed4bde704d8391d764d8a180d5
Michael de Hoog <michael.dehoog@ga.gov.au>
2010-04-27 05:42:00 +0000

    Removed synchronized methods from AnimatorSceneController, as they were causing deadlocks with AVListImpl (superclass of SceneController)
    git-svn-id: svn://localhost/Software/Java/WorldWind@627 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Animator/src/application/AnimatorSceneController.java
+++ Applications/Animator/src/application/AnimatorSceneController.java
@@ -11,52 +11,59 @@ public class AnimatorSceneController extends QualitySceneController
 	private boolean alpha = true;
 	private File screenshotFile = null;
 	private Object semaphore = new Object();
+	private Object semaphore2 = new Object();
 
 	@Override
-	public synchronized void doRepaint(DrawContext dc)
+	public void doRepaint(DrawContext dc)
 	{
-		super.doRepaint(dc);
-
-		if (screenshotFile != null)
+		synchronized (semaphore2)
 		{
-			if (!screenshotFile.getParentFile().exists())
-			{
-				screenshotFile.getParentFile().mkdirs();
-			}
-			try
-			{
-				int width = dc.getGLDrawable().getWidth();
-				int height = dc.getGLDrawable().getHeight();
-				if (screenshotFile.getName().toLowerCase().endsWith(".tga"))
-				{
-					/*com.sun.opengl.util.Screenshot.writeToTargaFile(screenshotFile,
-							width, height, true);*/
-					TGAScreenshot.writeToTargaFile(screenshotFile, width,
-							height, alpha);
-				}
-				else
-				{
-					com.sun.opengl.util.Screenshot.writeToFile(screenshotFile,
-							width, height, alpha);
-				}
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			}
+			super.doRepaint(dc);
 
-			synchronized (semaphore)
+			if (screenshotFile != null)
 			{
-				screenshotFile = null;
-				semaphore.notifyAll();
+				if (!screenshotFile.getParentFile().exists())
+				{
+					screenshotFile.getParentFile().mkdirs();
+				}
+				try
+				{
+					int width = dc.getGLDrawable().getWidth();
+					int height = dc.getGLDrawable().getHeight();
+					if (screenshotFile.getName().toLowerCase().endsWith(".tga"))
+					{
+						/*com.sun.opengl.util.Screenshot.writeToTargaFile(screenshotFile,
+								width, height, true);*/
+						TGAScreenshot.writeToTargaFile(screenshotFile, width,
+								height, alpha);
+					}
+					else
+					{
+						com.sun.opengl.util.Screenshot.writeToFile(
+								screenshotFile, width, height, alpha);
+					}
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+
+				synchronized (semaphore)
+				{
+					screenshotFile = null;
+					semaphore.notifyAll();
+				}
 			}
 		}
 	}
 
-	public synchronized void takeScreenshot(File screenshotFile, boolean alpha)
+	public void takeScreenshot(File screenshotFile, boolean alpha)
 	{
-		this.screenshotFile = screenshotFile;
-		this.alpha = alpha;
+		synchronized (semaphore2)
+		{
+			this.screenshotFile = screenshotFile;
+			this.alpha = alpha;
+		}
 	}
 
 	public void waitForScreenshot()
