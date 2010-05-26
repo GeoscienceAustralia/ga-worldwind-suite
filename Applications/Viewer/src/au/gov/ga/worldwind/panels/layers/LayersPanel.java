c621dd889e026097870ac33b004571e9055cda77
Michael de Hoog <michael.dehoog@ga.gov.au>
2010-05-26 02:11:23 +0000

    Added confirm dialog to layer deletion
    git-svn-id: svn://localhost/Software/Java/WorldWind@688 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Viewer/src/au/gov/ga/worldwind/panels/layers/LayersPanel.java
+++ Applications/Viewer/src/au/gov/ga/worldwind/panels/layers/LayersPanel.java
@@ -3,11 +3,15 @@ package au.gov.ga.worldwind.panels.layers;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.util.Collection;
 
 import javax.swing.DropMode;
 import javax.swing.Icon;
+import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
 import javax.swing.JToolBar;
 import javax.swing.JTree;
 import javax.swing.event.TreeModelEvent;
@@ -48,6 +52,7 @@ public class LayersPanel extends AbstractLayersPanel
 		});
 
 		enableActions();
+		createPopupMenu();
 	}
 
 	@Override
@@ -75,28 +80,14 @@ public class LayersPanel extends AbstractLayersPanel
 	@Override
 	protected void setupToolBarBeforeSlider(JToolBar toolBar)
 	{
-		newFolderAction = new BasicAction("Create", "Create Folder", Icons.newfolder.getIcon());
+		newFolderAction =
+				new BasicAction("New Folder", "Create New Folder", Icons.newfolder.getIcon());
 		newFolderAction.addActionListener(new ActionListener()
 		{
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				FolderNode node = new FolderNode("New Folder", Icons.folder.getURL(), true);
-				TreePath p = tree.getSelectionPath();
-				TreePath editPath;
-				if (p == null)
-				{
-					getModel().addToRoot(node, false);
-					editPath = new TreePath(new Object[] { root, node });
-				}
-				else
-				{
-					INode parent = (INode) p.getLastPathComponent();
-					getModel().insertNodeInto(node, parent, parent.getChildCount(), false);
-					editPath = p.pathByAddingChild(node);
-				}
-				tree.scrollPathToVisible(editPath);
-				editAtPath(editPath);
+				newFolder();
 			}
 		});
 
@@ -106,11 +97,7 @@ public class LayersPanel extends AbstractLayersPanel
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				TreePath p = tree.getSelectionPath();
-				if (p != null)
-				{
-					editAtPath(p);
-				}
+				rename();
 			}
 		});
 
@@ -120,14 +107,7 @@ public class LayersPanel extends AbstractLayersPanel
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				TreePath p = tree.getSelectionPath();
-				if (p != null)
-				{
-					INode node = (INode) p.getLastPathComponent();
-					getModel().removeNodeFromParent(node, true);
-					if (datasetPanel != null)
-						datasetPanel.getTree().repaint();
-				}
+				delete();
 			}
 		});
 
@@ -135,6 +115,89 @@ public class LayersPanel extends AbstractLayersPanel
 		toolBar.add(renameAction);
 		toolBar.add(deleteAction);
 		toolBar.addSeparator();
+	}
+
+	private void createPopupMenu()
+	{
+		final JPopupMenu popupMenu = new JPopupMenu();
+		popupMenu.add(renameAction);
+		popupMenu.add(deleteAction);
+		popupMenu.addSeparator();
+		popupMenu.add(newFolderAction);
+
+		tree.addMouseListener(new MouseAdapter()
+		{
+			@Override
+			public void mouseClicked(MouseEvent e)
+			{
+				int row = tree.getRowForLocation(e.getX(), e.getY());
+				if (row >= 0)
+				{
+					if (e.getButton() == MouseEvent.BUTTON3)
+					{
+						tree.setSelectionRow(row);
+						popupMenu.show(tree, e.getX(), e.getY());
+					}
+				}
+			}
+		});
+	}
+
+	private void newFolder()
+	{
+		FolderNode node = new FolderNode("New Folder", Icons.folder.getURL(), true);
+		TreePath p = tree.getSelectionPath();
+		TreePath editPath;
+		if (p == null)
+		{
+			getModel().addToRoot(node, false);
+			editPath = new TreePath(new Object[] { root, node });
+		}
+		else
+		{
+			INode parent = (INode) p.getLastPathComponent();
+			getModel().insertNodeInto(node, parent, parent.getChildCount(), false);
+			editPath = p.pathByAddingChild(node);
+		}
+		tree.scrollPathToVisible(editPath);
+		editAtPath(editPath);
+	}
+
+	private void rename()
+	{
+		TreePath p = tree.getSelectionPath();
+		if (p != null)
+		{
+			editAtPath(p);
+		}
+	}
+
+	private void delete()
+	{
+		TreePath p = tree.getSelectionPath();
+		if (p != null)
+		{
+			INode node = (INode) p.getLastPathComponent();
+			String type;
+			if (node instanceof ILayerNode)
+				type = "layer";
+			else
+				type = "folder";
+			String children = "";
+			if (node.getChildCount() > 0)
+				children = " and its children";
+			int choice =
+					JOptionPane.showConfirmDialog(this, "Are you sure you want to delete the "
+							+ type + " '" + node.getName() + "'" + children + "?",
+							"Confirm deletion", JOptionPane.YES_NO_OPTION,
+							JOptionPane.WARNING_MESSAGE);
+			if (choice == JOptionPane.YES_OPTION)
+			{
+				getModel().removeNodeFromParent(node, true);
+				if (datasetPanel != null)
+					datasetPanel.getTree().repaint();
+			}
+		}
 	}
 
 	private void enableActions()
