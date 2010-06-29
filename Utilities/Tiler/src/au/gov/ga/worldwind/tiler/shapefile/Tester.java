852daf37de214a8e04d756cbb6ffeb2c3d5430ca
Michael de Hoog <michael.dehoog@ga.gov.au>
2010-06-29 23:43:25 +0000

    git-svn-id: svn://localhost/Software/Java/WorldWind@766 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Utilities/Tiler/src/au/gov/ga/worldwind/tiler/shapefile/Tester.java
+++ Utilities/Tiler/src/au/gov/ga/worldwind/tiler/shapefile/Tester.java
@@ -9,17 +9,17 @@ public class Tester
 {
 	public static void main(String[] args)
 	{
-		//File input = new File("C:/WINNT/Profiles/u97852/Desktop/calcrete/calcrete.shp");
-		File input0 = new File("C:/WINNT/Profiles/u97852/Desktop/GSHHS_shp/c/GSHHS_c_L1.shp");
+		File input = new File("C:/WINNT/Profiles/u97852/Desktop/calcrete/calcrete.shp");
+		/*File input0 = new File("C:/WINNT/Profiles/u97852/Desktop/GSHHS_shp/c/GSHHS_c_L1.shp");
 		File input1 = new File("C:/WINNT/Profiles/u97852/Desktop/GSHHS_shp/l/GSHHS_l_L1.shp");
 		File input2 = new File("C:/WINNT/Profiles/u97852/Desktop/GSHHS_shp/i/GSHHS_i_L1.shp");
 		File input3 = new File("C:/WINNT/Profiles/u97852/Desktop/GSHHS_shp/h/GSHHS_h_L1.shp");
-		File input4 = new File("C:/WINNT/Profiles/u97852/Desktop/GSHHS_shp/f/GSHHS_f_L1.shp");
+		File input4 = new File("C:/WINNT/Profiles/u97852/Desktop/GSHHS_shp/f/GSHHS_f_L1.shp");*/
 		
 		File output = new File("C:/WINNT/Profiles/u97852/Desktop/calcrete/tiled");
 		double lzts = 36;
-		int level = 1;
-		File input = input1;
+		int level = 0;
+		//File input = input1;
 		
 		ProgressReporter progress = new Console.ConsoleProgressReporter();
 		ShapefileTiler.tile(input, output, level, lzts, progress);
