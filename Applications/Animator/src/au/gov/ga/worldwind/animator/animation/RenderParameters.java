9fdce353fa8771873d7181970ad623ea61ddcec5
James Navin <james.navin@ga.gov.au>
2010-08-19 07:30:10 +0000

    20100819 Fleshing out Animation interface and implementation
    git-svn-id: svn://localhost/Software/Java/WorldWind@826 4cd5884b-1955-4949-bbec-e4dd67779ed6

---
+++ Applications/Animator/src/au/gov/ga/worldwind/animator/animation/RenderParameters.java
@@ -0,0 +1,64 @@
+package au.gov.ga.worldwind.animator.animation;
+
+import java.awt.Dimension;
+
+/**
+ * A class that holds the render parameters associated with an animation 
+ * (e.g. frame rate, dimensions etc.)
+ * 
+ * @author James Navin (james.navin@ga.gov.au)
+ */
+public class RenderParameters
+{
+	private static final Dimension DEFAULT_DIMENSIONS = new Dimension(576, 1024);
+	
+	private static final int DEFAULT_FRAME_RATE = 25;
+	
+	/** 
+	 * The dimension of the output image (in pixels)
+	 * <p/>
+	 * Defaults to 
+	 */
+	private Dimension imageDimension = DEFAULT_DIMENSIONS;
+	
+	/** 
+	 * The frame rate of the output animation (in frames per second)
+	 * <p/>
+	 * Defaults to {@value #DEFAULT_FRAME_RATE} 
+	 */
+	private int frameRate = DEFAULT_FRAME_RATE;
+	
+	
+	/**
+	 * @return the {@link #imageDimension}
+	 */
+	public Dimension getImageDimension()
+	{
+		return imageDimension;
+	}
+	
+	/**
+	 * @param imageDimension the {@link #imageDimension} to set
+	 */
+	public void setImageDimension(Dimension imageDimension)
+	{
+		this.imageDimension = imageDimension;
+	}
+	
+	/**
+	 * @return the {@link #frameRate}
+	 */
+	public int getFrameRate()
+	{
+		return frameRate;
+	}
+	
+	/**
+	 * @param frameRate the {@link #frameRate} to set
+	 */
+	public void setFrameRate(int frameRate)
+	{
+		this.frameRate = frameRate;
+	}
+	
+}
