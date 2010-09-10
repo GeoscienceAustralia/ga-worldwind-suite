9dd3406f341dd1fa020844fd62146a84dfdb7ecf
Michael de Hoog <michael.dehoog@ga.gov.au>
2010-09-10 05:16:12 +0000

    Added getMessage() helper function to MessageSourceAccessor which calls it's MessageSource.getMessage()
    git-svn-id: svn://localhost/Software/Java/WorldWind@989 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Animator/src/au/gov/ga/worldwind/common/util/message/MessageSourceAccessor.java
+++ Applications/Animator/src/au/gov/ga/worldwind/common/util/message/MessageSourceAccessor.java
@@ -1,16 +1,17 @@
 package au.gov.ga.worldwind.common.util.message;
 
 /**
- * A static accessor to get access to the message source from anywhere in the application
+ * A static accessor to get access to the message source from anywhere in the
+ * application
  * 
  * @author James Navin (james.navin@ga.gov.au)
- *
+ * 
  */
 public class MessageSourceAccessor
 {
 
 	private static MessageSource messageSource;
-	
+
 	/**
 	 * @return The message source to use
 	 */
@@ -18,21 +19,36 @@ public class MessageSourceAccessor
 	{
 		if (messageSource == null)
 		{
-			throw new IllegalStateException("Message source not set. Call set() first to set the message source.");
+			throw new IllegalStateException(
+					"Message source not set. Call set() first to set the message source.");
 		}
-		
+
 		return messageSource;
 	}
 
 	/**
 	 * Set the message source to use in the application.
 	 * 
-	 * @param source The message source to set.
+	 * @param source
+	 *            The message source to set.
 	 */
 	public static void set(MessageSource source)
 	{
 		messageSource = source;
 	}
-	
-	
+
+	/**
+	 * Helper function that calls getMessage on the {@link MessageSource} linked
+	 * to by this Accessor.
+	 * 
+	 * @param key
+	 *            The key for the message to return
+	 * 
+	 * @return The message with the provided key, or <code>null</code> if a
+	 *         message with the provided key cannot be found
+	 */
+	public static String getMessage(String key)
+	{
+		return get().getMessage(key);
+	}
 }
