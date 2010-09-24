540b467a7a824558439fb5ef1f8e31c77c800f04
Michael de Hoog <michael.dehoog@ga.gov.au>
2010-09-24 06:42:27 +0000

    Added registration of the new ShadedElevationImageReaderDelegate
    git-svn-id: svn://localhost/Software/Java/WorldWind@1088 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Common/src/au/gov/ga/worldwind/common/layers/tiled/image/delegate/DelegateFactory.java
+++ Applications/Common/src/au/gov/ga/worldwind/common/layers/tiled/image/delegate/DelegateFactory.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import au.gov.ga.worldwind.common.layers.tiled.image.delegate.colortoalpha.ColorToAlphaTransformerDelegate;
 import au.gov.ga.worldwind.common.layers.tiled.image.delegate.nearestneighbor.NearestNeighborTextureTileFactoryDelegate;
+import au.gov.ga.worldwind.common.layers.tiled.image.delegate.shadedelevationreader.ShadedElevationImageReaderDelegate;
 import au.gov.ga.worldwind.common.layers.tiled.image.delegate.stripingfilter.StripingFilterTransformerDelegate;
 import au.gov.ga.worldwind.common.layers.tiled.image.delegate.transparentcolor.TransparentColorTransformerDelegate;
 
@@ -41,6 +42,7 @@ public class DelegateFactory
 		registerDelegate(ColorToAlphaTransformerDelegate.class);
 		registerDelegate(TransparentColorTransformerDelegate.class);
 		registerDelegate(StripingFilterTransformerDelegate.class);
+		registerDelegate(ShadedElevationImageReaderDelegate.class);
 	}
 
 	/**
