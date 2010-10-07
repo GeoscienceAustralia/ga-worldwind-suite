be6244c02198bf3204e4d6ac382a6474a3bf1219
Michael de Hoog <michael.dehoog@ga.gov.au>
2010-10-07 04:08:43 +0000

    Renamed spelling mistake
    git-svn-id: svn://localhost/Software/Java/WorldWind@1159 4cd5884b-1955-4949-bbec-e4dd67779ed6

---
+++ Applications/Animator/src/au/gov/ga/worldwind/animator/terrain/ImmediateRectangularTessellator.java
@@ -0,0 +1,20 @@
+package au.gov.ga.worldwind.animator.terrain;
+
+import gov.nasa.worldwind.render.DrawContext;
+import gov.nasa.worldwind.terrain.RectangularTessellator;
+import gov.nasa.worldwind.terrain.RectangularTessellatorAccessible;
+
+/**
+ * An extension of the {@link RectangularTessellator} that ignores the cache and
+ * regenerates vertices every repaint, instead of cacheing and regenerating
+ * every second.
+ */
+public class ImmediateRectangularTessellator extends RectangularTessellatorAccessible
+{
+	@Override
+	protected void makeVerts(DrawContext dc, RectTile tile)
+	{
+		RenderInfo ri = this.buildVerts(dc, tile, isMakeTileSkirts());
+		setRenderInfo(tile, ri);
+	}
+}
