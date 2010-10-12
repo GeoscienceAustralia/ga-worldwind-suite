f5c70e10712a02947a073b0180d4311d6c49a20a
James Navin <james.navin@ga.gov.au>
2010-10-12 22:11:26 +0000

    20101012 Fleshing out the camera path layer (CWW-4)
    git-svn-id: svn://localhost/Software/Java/WorldWind@1196 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Animator/src/au/gov/ga/worldwind/animator/layers/camerapath/EyePositionPath.java
+++ Applications/Animator/src/au/gov/ga/worldwind/animator/layers/camerapath/EyePositionPath.java
@@ -1,5 +1,6 @@
 package au.gov.ga.worldwind.animator.layers.camerapath;
 
+import gov.nasa.worldwind.geom.Position;
 import gov.nasa.worldwind.geom.Vec4;
 import gov.nasa.worldwind.render.DrawContext;
 import gov.nasa.worldwind.render.Material;
@@ -22,6 +23,7 @@ import au.gov.ga.worldwind.animator.animation.AnimationContext;
 import au.gov.ga.worldwind.animator.animation.AnimationContextImpl;
 import au.gov.ga.worldwind.animator.animation.KeyFrame;
 import au.gov.ga.worldwind.animator.animation.camera.Camera;
+import au.gov.ga.worldwind.common.util.HSLColor;
 
 import com.sun.opengl.util.BufferUtil;
 
@@ -30,38 +32,26 @@ import com.sun.opengl.util.BufferUtil;
  */
 class EyePositionPath implements Renderable
 {
-	private static final Color DEFAULT_CAMERA_PATH_COLOUR = Color.WHITE;
 	private static final Color DEFAULT_KEYFRAME_NODE_COLOUR = Color.BLUE;
 	
-	/** The number of frames currently held in the vertex buffers */
 	private int frameCount;
 	
-	/** The 'front' buffer, used to hold vertices ready for drawing */
-	private DoubleBuffer drawPathBuffer;
-	
-	/** The 'back' buffer, used to update vertices when a change is detected */
-	private DoubleBuffer updatePathBuffer;
-	
-	/** A lock used to synchronise access to the path buffers */
+	// Buffers used to draw the eye path
+	private DoubleBuffer pathVertexFrontBuffer;
+	private DoubleBuffer pathVertexBackBuffer;
+	private DoubleBuffer pathColourFrontBuffer;
+	private DoubleBuffer pathColourBackBuffer;
 	private Object pathBufferLock = new Object();
-
-	/** The centroid used as a reference for the path vertices */
 	private Vec4 pathReferenceCenter;
 	
-	/** A 'front' buffer for key frame markers */
-	private List<Marker> drawKeyFrameMarkers = new ArrayList<Marker>();
-	
-	/** A 'back' buffer for key frame markers */
-	private List<Marker> updateKeyFrameMarkers = new ArrayList<Marker>();
-	
-	/** A lock used to synchronise access to the key frame buffers */
+	// Buffers used to draw the key frame markers
+	private List<Marker> keyFrameMarkersFrontBuffer = new ArrayList<Marker>();
+	private List<Marker> keyFrameMarkersBackBuffer = new ArrayList<Marker>();
 	private Object keyFrameBufferLock = new Object();
 	
-	private MarkerRenderer markerRenderer = new MarkerRenderer();
+	private MarkerRenderer keyFramemarkerRenderer = new MarkerRenderer();
 	private MarkerAttributes keyFrameMarkerAttributes = new BasicMarkerAttributes();
 	
-	/** The colour to use to draw the camera path */
-	private Color cameraPathColour = DEFAULT_CAMERA_PATH_COLOUR;
 	private Color keyFrameNodeColour = DEFAULT_KEYFRAME_NODE_COLOUR;
 	
 	/** The animation whose camera path is to be displayed on this layer */
@@ -88,15 +78,17 @@ class EyePositionPath implements Renderable
 	
 	public void resetPath()
 	{
-		frameCount = animation.getFrameCount();
-		this.drawPathBuffer = BufferUtil.newDoubleBuffer(frameCount * 3);
-		this.updatePathBuffer = BufferUtil.newDoubleBuffer(frameCount * 3);
+		frameCount = animation.getFrameOfLastKeyFrame() - animation.getFrameOfFirstKeyFrame();
+		this.pathVertexFrontBuffer = BufferUtil.newDoubleBuffer(frameCount * 3);
+		this.pathVertexBackBuffer = BufferUtil.newDoubleBuffer(frameCount * 3);
+		this.pathColourFrontBuffer = BufferUtil.newDoubleBuffer(frameCount * 3);
+		this.pathColourBackBuffer = BufferUtil.newDoubleBuffer(frameCount * 3);
 	}
 	
 	@Override
 	public void render(DrawContext dc)
 	{
-		drawKeyFrameNodes(dc);
+		drawKeyFrameMarkers(dc);
 		drawPath(dc);
 	}
 
@@ -105,6 +97,11 @@ class EyePositionPath implements Renderable
 	 */
 	private void drawPath(DrawContext dc)
 	{
+		if (frameCount <= 0)
+		{
+			return;
+		}
+		
 		GL gl = dc.getGL();
 		gl.glPushClientAttrib(GL.GL_CLIENT_VERTEX_ARRAY_BIT);
 		gl.glPushAttrib(GL.GL_ALL_ATTRIB_BITS);
@@ -120,6 +117,7 @@ class EyePositionPath implements Renderable
 			// Points are drawn over the line to prevent gaps forming when 
 			// antialiasing and smoothing is applied to the line
 			gl.glEnableClientState(GL.GL_VERTEX_ARRAY);
+			gl.glEnableClientState(GL.GL_COLOR_ARRAY);
 			gl.glShadeModel(GL.GL_SMOOTH);
 			gl.glEnable(GL.GL_LINE_SMOOTH);
 			gl.glEnable(GL.GL_POINT_SMOOTH);
@@ -131,17 +129,17 @@ class EyePositionPath implements Renderable
 			gl.glPointSize(2.0f);
 			synchronized (pathBufferLock)
 			{
-				gl.glVertexPointer(3, GL.GL_DOUBLE, 0, drawPathBuffer);
+				gl.glColorPointer(3, GL.GL_DOUBLE, 0, pathColourFrontBuffer);
+				gl.glVertexPointer(3, GL.GL_DOUBLE, 0, pathVertexFrontBuffer);
 
 				// Draw a smooth line without depth testing, filling gaps with points
 				gl.glDepthMask(false);
-				gl.glColor3fv(cameraPathColour.getColorComponents(null), 0);
-				gl.glDrawArrays(GL.GL_LINE_STRIP, 0, animation.getFrameOfLastKeyFrame());
-				gl.glDrawArrays(GL.GL_POINTS, 0, animation.getFrameOfLastKeyFrame());
+				gl.glDrawArrays(GL.GL_LINE_STRIP, 0, frameCount);
+				gl.glDrawArrays(GL.GL_POINTS, 0, frameCount);
 				gl.glDepthMask(true);
 				
 				// Now redraw the line with depth testing to ensure line looks correct with markers
-				gl.glDrawArrays(GL.GL_LINE_STRIP, 0, animation.getFrameOfLastKeyFrame());
+				gl.glDrawArrays(GL.GL_LINE_STRIP, 0, frameCount);
 			}
 		}
 		finally
@@ -155,16 +153,10 @@ class EyePositionPath implements Renderable
 		}
 	}
 	
-	private void setupMarkerAttributes()
-	{
-		keyFrameMarkerAttributes.setMaterial(new Material(keyFrameNodeColour));
-		keyFrameMarkerAttributes.setMaxMarkerSize(200d);
-	}
-	
 	/**
-	 * Draw the camera eye position key frame nodes
+	 * Draw the camera eye position key frame markers
 	 */
-	private void drawKeyFrameNodes(DrawContext dc)
+	private void drawKeyFrameMarkers(DrawContext dc)
 	{
 		synchronized (keyFrameBufferLock)
 		{
@@ -174,74 +166,145 @@ class EyePositionPath implements Renderable
 			{
 				gl.glShadeModel(GL.GL_SMOOTH);
 				gl.glColor3fv(keyFrameNodeColour.getColorComponents(null), 0);
-				markerRenderer.render(dc, drawKeyFrameMarkers);
+				keyFramemarkerRenderer.render(dc, keyFrameMarkersFrontBuffer);
 			}
 			finally
 			{
 				gl.glPopAttrib();
 			}
-			
 		}
 	}
 	
 	/**
-	 * Populate the update vertex buffer with the coordinates of the 
+	 * Populate the update vertex buffers with the coordinates of the 
 	 * eye location at each frame in the animation.
 	 * <p/>
 	 * Vertex buffer has size <code>3*animation.getlastFrame()</code>, with <code>[x,y,z]</code> stored at 
 	 * <code>[3*frame, 3*frame+1, 3*frame+2]</code>
+	 * <p/>
+	 * Colour buffer has size <code>3*animation.getLastFrame()</code> with colours ramping through the HSV colour space
+	 * based on the rate of change of the camera eye position. Colours <code>[r,g,b]</code> are stored at <code>[3*frame, 3*frame+1, 3*frame+2]</code>.
 	 */
 	private void populatePathBuffers()
 	{
 		Camera renderCamera = animation.getCamera();
 		AnimationContext context = new AnimationContextImpl(animation);
 		pathReferenceCenter = null;
+		
+		Position previousEyePosition = null;
+		Position currentEyePosition = null;
+		double[] deltas = new double[animation.getFrameOfLastKeyFrame() - animation.getFrameOfFirstKeyFrame()];
+		double minDelta = Double.MAX_VALUE;
+		double maxDelta = 0d;
+		int i = 0;
+		pathVertexBackBuffer.rewind();
 		for (int frame = animation.getFrameOfFirstKeyFrame(); frame < animation.getFrameOfLastKeyFrame(); frame ++)
 		{
-			Vec4 eyeVector = context.getView().getGlobe().computePointFromPosition(renderCamera.getEyePositionAtFrame(context, frame));
+			currentEyePosition = renderCamera.getEyePositionAtFrame(context, frame);
+			
+			// Populate the vertex buffer
+			Vec4 eyeVector = context.getView().getGlobe().computePointFromPosition(currentEyePosition);
 			if (pathReferenceCenter == null)
 			{
 				pathReferenceCenter  = eyeVector; // Choose the first point in the path to be the reference point
 			}
-			updatePathBuffer.put(eyeVector.x - pathReferenceCenter.x);
-			updatePathBuffer.put(eyeVector.y - pathReferenceCenter.y);
-			updatePathBuffer.put(eyeVector.z - pathReferenceCenter.z);
+			pathVertexBackBuffer.put(eyeVector.x - pathReferenceCenter.x);
+			pathVertexBackBuffer.put(eyeVector.y - pathReferenceCenter.y);
+			pathVertexBackBuffer.put(eyeVector.z - pathReferenceCenter.z);
+			
+			// Populate the delta array
+			if (previousEyePosition != null)
+			{
+				double positionDelta = calculateDelta(currentEyePosition, previousEyePosition);
+				deltas[i] = positionDelta;
+				if (positionDelta > maxDelta)
+				{
+					maxDelta = positionDelta;
+				}
+				if (positionDelta < minDelta)
+				{
+					minDelta = positionDelta;
+				}
+			}
+			previousEyePosition = currentEyePosition;
+			i++;
 		}
-		updatePathBuffer.rewind();
+		
+		populatePathColourBufferFromDeltas(deltas, minDelta, maxDelta);
+		
+		pathVertexBackBuffer.rewind();
+	}
+
+	private double calculateDelta(Position currentPosition, Position previousPosition)
+	{
+		AnimationContext context = new AnimationContextImpl(animation);
+		Vec4 current = context.getView().getGlobe().computePointFromPosition(currentPosition);
+		Vec4 previous = context.getView().getGlobe().computePointFromPosition(previousPosition);
+		return Math.abs(current.distanceTo3(previous));
+	}
+
+	private void populatePathColourBufferFromDeltas(double[] deltas, double minDelta, double maxDelta)
+	{
+		double deltaWindow = maxDelta - minDelta;
+		if (deltaWindow < 1)
+		{
+			deltaWindow = 1;
+		}
+		
+		HSLColor hslColor = new HSLColor(0, 80, 50);
+		Color pathColor = null;
+		
+		pathColourBackBuffer.rewind();
+		for (int i = 0; i < deltas.length; i++)
+		{
+			pathColor = hslColor.adjustHue((float)((deltas[i] - minDelta) / deltaWindow) * 360f);
+			pathColourBackBuffer.put((double)pathColor.getRed() / 255d);
+			pathColourBackBuffer.put((double)pathColor.getGreen() / 255d);
+			pathColourBackBuffer.put((double)pathColor.getBlue() / 255d);
+		}
+		pathColourBackBuffer.rewind();
+	}
+	
+	private void setupMarkerAttributes()
+	{
+		keyFrameMarkerAttributes.setMaterial(new Material(keyFrameNodeColour));
+		keyFrameMarkerAttributes.setMaxMarkerSize(200d);
 	}
 	
 	private void populateKeyFrameMarkers()
 	{
-		updateKeyFrameMarkers.clear();
+		keyFrameMarkersBackBuffer.clear();
 		AnimationContext context = new AnimationContextImpl(animation);
 		for (KeyFrame keyFrame : animation.getKeyFrames())
 		{
 			if (isEyePositionFrame(keyFrame))
 			{
 				Marker marker = new BasicMarker(animation.getCamera().getEyePositionAtFrame(context, keyFrame.getFrame()), keyFrameMarkerAttributes);
-				updateKeyFrameMarkers.add(marker);
+				keyFrameMarkersBackBuffer.add(marker);
 			}
 		}
 	}
 	
-	/**
-	 * Swap the two buffers 
-	 */
 	private void swapBuffers()
 	{
 		synchronized (pathBufferLock)
 		{
-			DoubleBuffer tmp = drawPathBuffer;
-			drawPathBuffer = updatePathBuffer;
-			updatePathBuffer = tmp;
-			drawPathBuffer.rewind();
+			DoubleBuffer tmp = pathVertexFrontBuffer;
+			pathVertexFrontBuffer = pathVertexBackBuffer;
+			pathVertexBackBuffer = tmp;
+			pathVertexFrontBuffer.rewind();
+			
+			tmp = pathColourFrontBuffer;
+			pathColourFrontBuffer = pathColourBackBuffer;
+			pathColourBackBuffer = tmp;
+			pathColourFrontBuffer.rewind();
 		}
 		
 		synchronized (keyFrameBufferLock)
 		{
-			List<Marker> tmp = drawKeyFrameMarkers;
-			drawKeyFrameMarkers = updateKeyFrameMarkers;
-			updateKeyFrameMarkers = tmp;
+			List<Marker> tmp = keyFrameMarkersFrontBuffer;
+			keyFrameMarkersFrontBuffer = keyFrameMarkersBackBuffer;
+			keyFrameMarkersBackBuffer = tmp;
 		}
 	}
 	
