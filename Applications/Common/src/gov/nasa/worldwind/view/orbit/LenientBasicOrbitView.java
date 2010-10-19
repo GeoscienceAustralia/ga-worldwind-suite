6a553a2861861d923ea8d509de479aa298260775
James Navin <james.navin@ga.gov.au>
2010-10-19 23:10:30 +0000

    20101020 Changing allowable pitches in LenientBasicOrbitView to accept in the range [-180,180]
    git-svn-id: svn://localhost/Software/Java/WorldWind@1233 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Common/src/gov/nasa/worldwind/view/orbit/LenientBasicOrbitView.java
+++ Applications/Common/src/gov/nasa/worldwind/view/orbit/LenientBasicOrbitView.java
@@ -2,6 +2,9 @@ package gov.nasa.worldwind.view.orbit;
 
 import gov.nasa.worldwind.view.orbit.OrbitViewInputSupport.OrbitViewState;
 
+/**
+ * A more lenient orbit view that allows a greater range of viewing angles.
+ */
 public class LenientBasicOrbitView extends BasicOrbitView
 {
 	@Override
@@ -13,8 +16,8 @@ public class LenientBasicOrbitView extends BasicOrbitView
 	            && modelCoords.getCenterPosition().getLatitude().degrees <= 90
 	            && modelCoords.getHeading() != null
 	            && modelCoords.getPitch() != null
-	            && modelCoords.getPitch().degrees >= -90
-	            && modelCoords.getPitch().degrees <= 90
+	            && modelCoords.getPitch().degrees >= -180
+	            && modelCoords.getPitch().degrees <= 180
 	            && modelCoords.getZoom() >= 0);
 	}
 }
