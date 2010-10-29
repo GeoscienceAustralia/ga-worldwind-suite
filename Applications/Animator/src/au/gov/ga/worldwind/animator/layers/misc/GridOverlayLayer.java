f368230390437f0426ce63b6a9b3f30673462c2d
Michael de Hoog <michael.dehoog@ga.gov.au>
2010-10-29 00:00:28 +0000

    Modified the GridOverlayLayer to use the full viewport dimensions instead of just the width/height, so that it supports non-zerod viewports
    git-svn-id: svn://localhost/Software/Java/WorldWind@1282 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Animator/src/au/gov/ga/worldwind/animator/layers/misc/GridOverlayLayer.java
+++ Applications/Animator/src/au/gov/ga/worldwind/animator/layers/misc/GridOverlayLayer.java
@@ -21,31 +21,31 @@ import com.sun.opengl.util.BufferUtil;
 /**
  * A layer that renders a grid overlay on top of the viewport window
  * <p/>
- * The grid is centred in the x- and y-dimensions, and grid spacing can be configured.
+ * The grid is centred in the x- and y-dimensions, and grid spacing can be
+ * configured.
  */
 public class GridOverlayLayer extends AbstractLayer
 {
 	private static final float LINE_THICKNESS = 0.8f;
 	private static final double DEFAULT_OPACITY = 0.4;
-	
+
 	// Grid colour
 	private static final float[] DEFAULT_GRID_COLOR = Color.WHITE.getColorComponents(null);
 	private float[] gridColor = DEFAULT_GRID_COLOR;
-	
+
 	// Grid spacing (in percent)
 	private static final double DEFAULT_VERTICAL_SPACING = 0.2;
 	private static final double DEFAULT_HORIZONTAL_SPACING = 0.2;
 	private double verticalSpacing = DEFAULT_VERTICAL_SPACING;
 	private double horizontalSpacing = DEFAULT_HORIZONTAL_SPACING;
-	
-	// Used to detect a change in the viewport
-	private int viewportWidth = -1;
-	private int viewportHeight = -1;
-	
+
+	// Used to detect changes in the viewport
+	private Rectangle lastViewport = null;
+
 	// Buffer that holds the grid line end points, for drawing
 	private DoubleBuffer gridBuffer;
 	private int numberOfBufferEntries;
-	
+
 	// Grid overlay implemented as an ordered renderable with depth 0
 	private OrderedRenderable gridOverlay = new OrderedRenderable()
 	{
@@ -54,22 +54,22 @@ public class GridOverlayLayer extends AbstractLayer
 		{
 			drawGrid(dc);
 		}
-		
+
 		@Override
 		public void pick(DrawContext dc, Point pickPoint)
 		{
 			// Picking not implemented
 		}
-		
+
 		@Override
 		public double getDistanceFromEye()
 		{
 			return 0;
 		}
 	};
-	
+
 	private boolean centreGrid = true;
-	
+
 	/**
 	 * Create a new grid overlay that is centred with a default grid spacing.
 	 */
@@ -77,31 +77,39 @@ public class GridOverlayLayer extends AbstractLayer
 	{
 		setOpacity(DEFAULT_OPACITY);
 	}
-	
+
 	/**
-	 * Create a new grid overlay that is configured according to the provided parameters
+	 * Create a new grid overlay that is configured according to the provided
+	 * parameters
 	 * 
-	 * @param centred Whether or not to centre the grid in the x- and y-axes.
-	 * @param verticalSpacing The vertical spacing to apply to the grid (in percent, range <code>(0,1)</code>)
-	 * @param horizontalSpacing The vertical spacing to apply to the grid (in percent, range <code>(0,1)</code>)
+	 * @param centred
+	 *            Whether or not to centre the grid in the x- and y-axes.
+	 * @param verticalSpacing
+	 *            The vertical spacing to apply to the grid (in percent, range
+	 *            <code>(0,1)</code>)
+	 * @param horizontalSpacing
+	 *            The vertical spacing to apply to the grid (in percent, range
+	 *            <code>(0,1)</code>)
 	 */
 	public GridOverlayLayer(boolean centred, double verticalSpacing, double horizontalSpacing)
 	{
 		this();
-		Validate.isTrue(verticalSpacing > 0 && verticalSpacing < 1, "Vertical spacing must be in the range (0,1)");
-		Validate.isTrue(horizontalSpacing > 0 && horizontalSpacing < 1, "Horizontal spacing must be in the range (0,1)");
-		
+		Validate.isTrue(verticalSpacing > 0 && verticalSpacing < 1,
+				"Vertical spacing must be in the range (0,1)");
+		Validate.isTrue(horizontalSpacing > 0 && horizontalSpacing < 1,
+				"Horizontal spacing must be in the range (0,1)");
+
 		centreGrid = centred;
 		this.verticalSpacing = verticalSpacing;
 		this.horizontalSpacing = horizontalSpacing;
 	}
-	
+
 	@Override
 	protected void doRender(DrawContext dc)
 	{
 		gridOverlay.render(dc);
 	}
-	
+
 	/**
 	 * Draw the grid overlay to the draw context
 	 */
@@ -109,52 +117,52 @@ public class GridOverlayLayer extends AbstractLayer
 	{
 		GL gl = dc.getGL();
 		GLU glu = dc.getGLU();
-		
+
 		boolean clientAttribsPushed = false;
 		boolean attribsPushed = false;
 		boolean projectionPushed = false;
 		boolean modelviewPushed = false;
-		
+
 		Rectangle viewport = dc.getView().getViewport();
-		
+
 		try
 		{
 			recalculateGrid(viewport);
-			
+
 			gl.glPushClientAttrib(GL.GL_CLIENT_VERTEX_ARRAY_BIT);
 			clientAttribsPushed = true;
-			
+
 			gl.glPushAttrib(GL.GL_ALL_ATTRIB_BITS);
 			attribsPushed = true;
-			
+
 			gl.glMatrixMode(GL.GL_PROJECTION);
 			gl.glPushMatrix();
 			gl.glLoadIdentity();
 			glu.gluOrtho2D(0, viewport.getWidth(), 0, viewport.getHeight());
 			projectionPushed = true;
-			
+
 			gl.glMatrixMode(GL.GL_MODELVIEW);
-            gl.glPushMatrix();
+			gl.glPushMatrix();
 			modelviewPushed = true;
-            gl.glLoadIdentity();
-            
-            gl.glColor4d(gridColor[0], gridColor[1], gridColor[2], getOpacity());
-            gl.glEnable(GL.GL_BLEND);
-            gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
-            gl.glDisable(GL.GL_DEPTH_TEST);
-            gl.glEnableClientState(GL.GL_VERTEX_ARRAY);
-            
-            gl.glLineWidth(LINE_THICKNESS);
-            gl.glVertexPointer(2, GL.GL_DOUBLE, 0, gridBuffer.rewind());
-            gl.glDrawArrays(GL.GL_LINES, 0, numberOfBufferEntries);
+			gl.glLoadIdentity();
+
+			gl.glColor4d(gridColor[0], gridColor[1], gridColor[2], getOpacity());
+			gl.glEnable(GL.GL_BLEND);
+			gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
+			gl.glDisable(GL.GL_DEPTH_TEST);
+			gl.glEnableClientState(GL.GL_VERTEX_ARRAY);
+
+			gl.glLineWidth(LINE_THICKNESS);
+			gl.glVertexPointer(2, GL.GL_DOUBLE, 0, gridBuffer.rewind());
+			gl.glDrawArrays(GL.GL_LINES, 0, numberOfBufferEntries);
 		}
 		finally
 		{
 			if (modelviewPushed)
-            {
-                gl.glMatrixMode(GL.GL_MODELVIEW);
-                gl.glPopMatrix();
-            }
+			{
+				gl.glMatrixMode(GL.GL_MODELVIEW);
+				gl.glPopMatrix();
+			}
 			if (projectionPushed)
 			{
 				gl.glMatrixMode(GL.GL_PROJECTION);
@@ -172,22 +180,23 @@ public class GridOverlayLayer extends AbstractLayer
 	}
 
 	/**
-	 * Recalculate the grid lines if the viewport dimensions have changed since the last calculation
+	 * Recalculate the grid lines if the viewport dimensions have changed since
+	 * the last calculation
 	 */
 	private void recalculateGrid(Rectangle viewport)
 	{
-		if (!viewportHasChanged(viewport))
+		if (this.lastViewport != null && this.lastViewport.equals(viewport))
 		{
+			//viewport hasn't changed since last time, so don't need to recalculate
 			return;
 		}
-		viewportWidth = (int) viewport.getWidth();
-		viewportHeight = (int) viewport.getHeight();
+		this.lastViewport = viewport;
 
 		List<Point> tempGridBuffer = new ArrayList<Point>();
-		
+
 		calculateVerticalGridLines(viewport, tempGridBuffer);
 		calculateHorizontalGridLines(viewport, tempGridBuffer);
-		
+
 		numberOfBufferEntries = tempGridBuffer.size() * 2;
 		gridBuffer = BufferUtil.newDoubleBuffer(numberOfBufferEntries);
 		for (Point gridPoint : tempGridBuffer)
@@ -212,54 +221,43 @@ public class GridOverlayLayer extends AbstractLayer
 			calculateNonCentredHorizontalGridLines(viewport, gridBuffer);
 		}
 	}
-	
+
 	private void calculateCentredHorizontalGridLines(Rectangle viewport, List<Point> gridBuffer)
 	{
-		addHorizontalLineAt((int)viewport.getCenterY(), gridBuffer);
-		
-		int deltaY = (int)(viewportHeight * getVerticalSpacing());
-		
+		int centerY = (int) viewport.getCenterY();
+		addHorizontalLineAt(centerY, viewport, gridBuffer);
+
+		int deltaY = (int) (viewport.height * getVerticalSpacing());
+
 		// Add lines below the centre
-		int lineY = (int)viewport.getCenterY();
-		while (lineY > 0)
+		for (int lineY = centerY - deltaY; lineY > viewport.y; lineY -= deltaY)
 		{
-			lineY -= deltaY;
-			if (lineY > 0)
-			{
-				addHorizontalLineAt(lineY, gridBuffer);
-			}
+			addHorizontalLineAt(lineY, viewport, gridBuffer);
 		}
-		
+
 		// Add lines above the centre
-		lineY = (int)viewport.getCenterY();
-		while (lineY < viewportHeight)
+		for (int lineY = centerY + deltaY; lineY < viewport.y + viewport.height; lineY += deltaY)
 		{
-			lineY += deltaY;
-			if (lineY < viewportHeight)
-			{
-				addHorizontalLineAt(lineY, gridBuffer);
-			}
+			addHorizontalLineAt(lineY, viewport, gridBuffer);
 		}
 	}
-	
+
 	private void calculateNonCentredHorizontalGridLines(Rectangle viewport, List<Point> gridBuffer)
 	{
-		int deltaY = (int)(viewportHeight * getVerticalSpacing());
-		
-		int lineY = 0;
-		while (lineY <= viewportHeight)
+		int deltaY = (int) (viewport.height * getVerticalSpacing());
+
+		for (int lineY = viewport.y; lineY <= viewport.y + viewport.height; lineY += deltaY)
 		{
-			lineY += deltaY;
-			addHorizontalLineAt(lineY, gridBuffer);
+			addHorizontalLineAt(lineY, viewport, gridBuffer);
 		}
 	}
-	
-	private void addHorizontalLineAt(int y, List<Point> gridBuffer)
+
+	private void addHorizontalLineAt(int y, Rectangle viewport, List<Point> gridBuffer)
 	{
-		gridBuffer.add(new Point(0, y));
-		gridBuffer.add(new Point(viewportWidth, y));
+		gridBuffer.add(new Point(viewport.x, y));
+		gridBuffer.add(new Point(viewport.x + viewport.width, y));
 	}
-	
+
 	/**
 	 * Add vertical grid lines to the grid
 	 */
@@ -274,52 +272,40 @@ public class GridOverlayLayer extends AbstractLayer
 			calculateNonCentredVerticalGridLines(viewport, gridBuffer);
 		}
 	}
-	
+
 	private void calculateCentredVerticalGridLines(Rectangle viewport, List<Point> gridBuffer)
 	{
-		addVerticalLineAt((int)viewport.getCenterX(), gridBuffer);
-		
-		int deltaX = (int)(viewportWidth * getHorizontalSpacing());
-		
+		int centerX = (int) viewport.getCenterX() - 1; //move to the left 1 pixel so it lines up with the crosshair
+		addVerticalLineAt(centerX, viewport, gridBuffer);
+
+		int deltaX = (int) (viewport.width * getHorizontalSpacing());
+
 		// Add lines to the left of centre
-		int lineX = (int)viewport.getCenterX();
-		while (lineX > 0)
+		for (int lineX = centerX - deltaX; lineX > viewport.x; lineX -= deltaX)
 		{
-			lineX -= deltaX;
-			if (lineX > 0)
-			{
-				addVerticalLineAt(lineX, gridBuffer);
-			}
+			addVerticalLineAt(lineX, viewport, gridBuffer);
 		}
-		
+
 		// Add lines to the right of centre
-		lineX = (int)viewport.getCenterX();
-		while (lineX < viewportWidth)
+		for (int lineX = centerX + deltaX; lineX < viewport.x + viewport.width; lineX += deltaX)
 		{
-			lineX += deltaX;
-			if (lineX < viewportWidth)
-			{
-				addVerticalLineAt(lineX, gridBuffer);
-			}
+			addVerticalLineAt(lineX, viewport, gridBuffer);
 		}
 	}
-	
+
 	private void calculateNonCentredVerticalGridLines(Rectangle viewport, List<Point> gridBuffer)
 	{
-		int deltaX = (int)(viewportWidth * getHorizontalSpacing());
-		
-		int lineX = 0;
-		while (lineX <= viewportWidth)
+		int deltaX = (int) (viewport.width * getHorizontalSpacing());
+		for (int lineX = viewport.x; lineX <= viewport.x + viewport.width; lineX += deltaX)
 		{
-			lineX += deltaX;
-			addVerticalLineAt(lineX, gridBuffer);
+			addVerticalLineAt(lineX, viewport, gridBuffer);
 		}
 	}
-	
-	private void addVerticalLineAt(int x, List<Point> gridBuffer)
+
+	private void addVerticalLineAt(int x, Rectangle viewport, List<Point> gridBuffer)
 	{
-		gridBuffer.add(new Point(x, 0));
-		gridBuffer.add(new Point(x, viewportHeight));
+		gridBuffer.add(new Point(x, viewport.y));
+		gridBuffer.add(new Point(x, viewport.y + viewport.height));
 	}
 
 	public double getVerticalSpacing()
@@ -343,12 +329,12 @@ public class GridOverlayLayer extends AbstractLayer
 		this.horizontalSpacing = horizontalSpacing;
 		markChanged();
 	}
-	
+
 	public Color getGridColor()
 	{
 		return new Color(gridColor[0], gridColor[1], gridColor[2]);
 	}
-	
+
 	public void setGridColor(Color color)
 	{
 		if (color != null)
@@ -356,16 +342,10 @@ public class GridOverlayLayer extends AbstractLayer
 			this.gridColor = color.getColorComponents(null);
 		}
 	}
-	
+
 	private void markChanged()
 	{
 		// hijack the viewport check to mark the grid as changed
-		viewportWidth = -1;
-	}
-	
-	private boolean viewportHasChanged(Rectangle viewport)
-	{
-		return (int)viewport.getWidth() != viewportWidth || 
-			   (int)viewport.getHeight() != viewportHeight;
+		this.lastViewport = null;
 	}
 }
