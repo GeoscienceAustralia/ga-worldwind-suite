98ec7d9fdd7f39b57f44ef1d38859a150f6045a8
James Navin <james.navin@ga.gov.au>
2010-11-23 21:46:19 +0000

    20101123 Adding country boundaries to the layer preview window(CWW-99)
    git-svn-id: svn://localhost/Software/Java/WorldWind@1407 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Animator/src/au/gov/ga/worldwind/wmsbrowser/WmsLayerInfoPanel.java
+++ Applications/Animator/src/au/gov/ga/worldwind/wmsbrowser/WmsLayerInfoPanel.java
@@ -2,61 +2,133 @@ package au.gov.ga.worldwind.wmsbrowser;
 
 import static au.gov.ga.worldwind.common.util.message.MessageSourceAccessor.getMessage;
 import static au.gov.ga.worldwind.wmsbrowser.util.message.WmsBrowserMessageConstants.*;
+import gov.nasa.worldwind.BasicModel;
 import gov.nasa.worldwind.avlist.AVKey;
+import gov.nasa.worldwind.awt.AWTInputHandler;
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.LatLon;
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.geom.Sector;
+import gov.nasa.worldwind.globes.EarthFlat;
+import gov.nasa.worldwind.layers.WorldMapLayer;
 import gov.nasa.worldwind.ogc.wms.WMSCapabilities;
 import gov.nasa.worldwind.ogc.wms.WMSLayerCapabilities;
 import gov.nasa.worldwind.ogc.wms.WMSLayerInfoURL;
+import gov.nasa.worldwind.view.orbit.OrbitView;
+import gov.nasa.worldwind.wms.WMSTiledImageLayer;
 import gov.nasa.worldwindow.core.WMSLayerInfo;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.ScrollPane;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.util.Iterator;
 
 import javax.swing.Box;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 
+import nasa.worldwind.awt.WorldWindowGLCanvas;
+import au.gov.ga.worldwind.animator.application.AnimatorConfiguration;
 import au.gov.ga.worldwind.common.util.Util;
+import au.gov.ga.worldwind.wmsbrowser.layer.MetacartaCoastlineLayer;
+import au.gov.ga.worldwind.wmsbrowser.layer.MetacartaCountryBoundariesLayer;
 
 /**
  * A panel used to display WMS Layer information
+ * <p/>
+ * Uses the capabilities information to display layer metadata, and a flat globe view to display the layer data itself
  */
 public class WmsLayerInfoPanel extends JComponent
 {
+	private static final Sector DEFAULT_VIEW_EXTENTS = new Sector(Angle.NEG90, Angle.POS90, Angle.NEG180, Angle.POS180);
 	private static final long serialVersionUID = 20101122L;
 	private static int PADDING = 10;
 	
 	/** The layer this panel is backed by */
 	private WMSLayerInfo layerInfo;
+	private WMSLayerCapabilities layerCapabilities;
+	
+	private JPanel splitPanel;
 	
 	private JPanel panel;
 	
 	private int currentRow = 0;
 	
+	private WorldWindowGLCanvas wwd;
+	private WorldMapLayer worldMapLayer;
+	private MetacartaCountryBoundariesLayer countryBoundariesLayer;
+	private MetacartaCoastlineLayer coastlinesLayer;
+	
 	public WmsLayerInfoPanel()
 	{
 		setLayout(new BorderLayout());
 		
+		initialiseSplitPanel();
+		initialisePanel();
+		initialiseWorldWindow();
+		
+		addComponentListener(new ComponentAdapter()
+		{
+			@Override
+			public void componentShown(ComponentEvent e)
+			{
+				wwd.createBufferStrategy(2);
+			}
+		});
+	}
+
+	private void initialiseSplitPanel()
+	{
+		splitPanel = new JPanel();
+		splitPanel.setLayout(new GridLayout(2, 1));
+		splitPanel.setBorder(null);
+		add(splitPanel, BorderLayout.CENTER);
+	}
+
+	private void initialisePanel()
+	{
 		panel = new JPanel();
 		panel.setLayout(new GridBagLayout());
 		panel.setBorder(new CompoundBorder(panel.getBorder(), new EmptyBorder(PADDING, PADDING, PADDING, PADDING)));
 		panel.setBackground(Color.WHITE);
 		panel.setOpaque(true);
 		
-		JScrollPane scrollPane = new JScrollPane(panel);
-		add(scrollPane, BorderLayout.CENTER);
+		ScrollPane scrollPane = new ScrollPane();
+		scrollPane.add(panel);
+		splitPanel.add(scrollPane);
+	}
+	
+	private void initialiseWorldWindow()
+	{
+		wwd = new WorldWindowGLCanvas(AnimatorConfiguration.getGLCapabilities());
+		wwd.setModel(new BasicModel());
+		((AWTInputHandler) wwd.getInputHandler()).setSmoothViewChanges(false);
+		wwd.setMinimumSize(new Dimension(1, 1));
+		((OrbitView) wwd.getView()).getOrbitViewLimits().setPitchLimits(Angle.ZERO, Angle.ZERO);
+		((OrbitView) wwd.getView()).getOrbitViewLimits().setHeadingLimits(Angle.ZERO, Angle.ZERO);
+		wwd.getModel().setGlobe(new EarthFlat());
+		wwd.setVisible(false);
+		
+		splitPanel.add(wwd);
+		
+		worldMapLayer = new WorldMapLayer();
+		countryBoundariesLayer = new MetacartaCountryBoundariesLayer();
+		coastlinesLayer = new MetacartaCoastlineLayer();
 	}
 	
 	public void setLayerInfo(WMSLayerInfo layerInfo)
@@ -77,6 +149,7 @@ public class WmsLayerInfoPanel extends JComponent
 			panel.removeAll();
 			if (layerInfo == null)
 			{
+				wwd.setVisible(false);
 				return;
 			}
 			
@@ -84,23 +157,26 @@ public class WmsLayerInfoPanel extends JComponent
 			
 			addHeading(layerInfo.getTitle());
 			
-			WMSLayerCapabilities capabilities = ((WMSCapabilities)layerInfo.getCaps()).getLayerByName(layerInfo.getParams().getStringValue(AVKey.LAYER_NAMES));
-			if (capabilities == null)
+			layerCapabilities = ((WMSCapabilities)layerInfo.getCaps()).getLayerByName(layerInfo.getParams().getStringValue(AVKey.LAYER_NAMES));
+			if (layerCapabilities == null)
 			{
 				addSubHeading(getMessage(getLayerInfoNoCapabilitiesMsgKey()));
 			}
 			
-			addNameValuePair(getMessage(getLayerInfoDataUrlKey()), getDataUrlAsString(capabilities));
-			addNameValuePair(getMessage(getLayerInfoMetaDataUrlKey()), getMetaDataUrlAsString(capabilities));
-			addNameValuePair(getMessage(getLayerInfoLastUpdateKey()), capabilities.getLastUpdate());
-			addNameValuePair(getMessage(getLayerInfoAbstractKey()), capabilities.getLayerAbstract());
-			addNameValuePair(getMessage(getLayerInfoBoundingBoxKey()), getBoundingBoxAsString(capabilities));
-			addNameValuePair(getMessage(getLayerInfoKeywordsKey()), getKeyWordsAsString(capabilities));
+			addNameValuePair(getMessage(getLayerInfoDataUrlKey()), getDataUrlAsString(layerCapabilities));
+			addNameValuePair(getMessage(getLayerInfoMetaDataUrlKey()), getMetaDataUrlAsString(layerCapabilities));
+			addNameValuePair(getMessage(getLayerInfoLastUpdateKey()), layerCapabilities.getLastUpdate());
+			addNameValuePair(getMessage(getLayerInfoAbstractKey()), layerCapabilities.getLayerAbstract());
+			addNameValuePair(getMessage(getLayerInfoBoundingBoxKey()), getBoundingBoxAsString(layerCapabilities));
+			addNameValuePair(getMessage(getLayerInfoKeywordsKey()), getKeyWordsAsString(layerCapabilities));
+			addEndingSpace();
 			
-			addEndPadding();
+			updateFlatGlobeViewer();
 		}
 		finally
 		{
+			panel.validate();
+			panel.repaint();
 			validate();
 			repaint();
 		}
@@ -187,11 +263,42 @@ public class WmsLayerInfoPanel extends JComponent
 		currentRow++;
 	}
 	
-	private void addEndPadding()
+	private void addEndingSpace()
 	{
 		panel.add(Box.createVerticalGlue(), createEndConstraints());
 	}
 	
+	private void updateFlatGlobeViewer()
+	{
+		wwd.setVisible(true);
+		wwd.getModel().getLayers().clear();		
+		wwd.getModel().getLayers().add(new WMSTiledImageLayer((WMSCapabilities)layerInfo.getCaps(), layerInfo.getParams()));
+		wwd.getModel().getLayers().add(countryBoundariesLayer);
+		wwd.getModel().getLayers().add(coastlinesLayer);
+		wwd.getModel().getLayers().add(worldMapLayer);
+		zoomViewToSector(layerCapabilities.getGeographicBoundingBox() == null ? DEFAULT_VIEW_EXTENTS : layerCapabilities.getGeographicBoundingBox());
+		wwd.redraw();
+	}
+	
+	private void zoomViewToSector(Sector geographicBoundingBox)
+	{
+		// Estimate the earth as a perfect sphere...
+		double earthRadius = wwd.getModel().getGlobe().getRadius();
+		
+		// Compute the estimated deltas in metres (Add some padding to account for rounding errors and some buffering)
+		double deltaX = earthRadius * geographicBoundingBox.getDeltaLonRadians();
+		double deltaY = earthRadius * geographicBoundingBox.getDeltaLatRadians();
+
+		// Compute the altitude required to ensure that the whole of each axis is visible in the viewport 
+		double altitudeX = (deltaX/2) / Math.tan(wwd.getView().getFieldOfView().radians / 2) * 1.3;
+		double altitudeY = (deltaY/2) / Math.tan(wwd.getView().getFieldOfView().radians / 2) * 1.3;
+		
+		LatLon centroid = geographicBoundingBox.getCentroid();
+		Position pos = new Position(centroid.getLatitude(), centroid.getLongitude(), Math.max(altitudeX, altitudeY));
+		
+		wwd.getView().setEyePosition(pos);
+	}
+
 	private static String getStringValue(String value)
 	{
 		return Util.isBlank(value) ? getMessage(getLayerInfoDefaultStringValueKey()) : value;
@@ -213,6 +320,7 @@ public class WmsLayerInfoPanel extends JComponent
 	{
 		GridBagConstraints constraints = createElementConstraints();
 		constraints.weighty = 1;
+		constraints.fill = GridBagConstraints.BOTH;
 		return constraints;
 	}
 	
---
+++ Applications/Animator/src/au/gov/ga/worldwind/wmsbrowser/layer/MetacartaCountryBoundariesLayer.java
@@ -0,0 +1,40 @@
+package au.gov.ga.worldwind.wmsbrowser.layer;
+
+import gov.nasa.worldwind.render.DrawContext;
+
+import javax.media.opengl.GL;
+
+import au.gov.ga.worldwind.animator.layers.immediate.ImmediateBasicTiledImageLayer;
+
+public class MetacartaCountryBoundariesLayer extends ImmediateBasicTiledImageLayer
+{
+	public MetacartaCountryBoundariesLayer()
+	{
+		super(MetacartaLayerUtil.makeLevels("Earth/Metacarta Country Boundaries",
+				"country_02"));
+		setUseTransparentTextures(true);
+		setUseMipMaps(true);
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Metacarta country boundaries";
+	}
+	
+	@Override
+	public void setSplitScale(double splitScale)
+	{
+		super.setSplitScale(splitScale);
+	}
+	
+	@Override
+	protected void setBlendingFunction(DrawContext dc)
+	{
+		GL gl = dc.getGL();
+		double alpha = this.getOpacity();
+		gl.glColor4d(alpha, alpha, alpha, alpha);
+		gl.glEnable(GL.GL_BLEND);
+		gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
+	}
+}
