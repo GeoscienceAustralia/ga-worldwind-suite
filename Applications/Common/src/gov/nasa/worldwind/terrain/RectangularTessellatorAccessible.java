449f70bd0969cb9f499bcfe1349e57e8e40b46bf
Michael de Hoog <michael.dehoog@ga.gov.au>
2010-11-23 01:49:37 +0000

    Made some more of the Tessellator accessible
    git-svn-id: svn://localhost/Software/Java/WorldWind@1404 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Common/src/gov/nasa/worldwind/terrain/RectangularTessellatorAccessible.java
+++ Applications/Common/src/gov/nasa/worldwind/terrain/RectangularTessellatorAccessible.java
@@ -1,6 +1,7 @@
 package gov.nasa.worldwind.terrain;
 
 import gov.nasa.worldwind.geom.Vec4;
+import gov.nasa.worldwind.render.DrawContext;
 
 import java.nio.DoubleBuffer;
 import java.nio.IntBuffer;
@@ -12,9 +13,24 @@ public class RectangularTessellatorAccessible extends RectangularTessellator
 		return tile.ri;
 	}
 	
+	public static int getLevel(RectTile tile)
+	{
+		return tile.level;
+	}
+	
+	public static int getDensity(RectTile tile)
+	{
+		return tile.density;
+	}
+	
 	public static void setRenderInfo(RectTile tile, RenderInfo ri)
 	{
 		tile.ri = ri;
+	}
+	
+	public static RenderInfo createRenderInfo(DrawContext dc, int density, DoubleBuffer vertices, Integer verticesBuffer, Vec4 refCenter)
+	{
+		return new RenderInfo(dc, density, vertices, verticesBuffer, refCenter);
 	}
 
 	public static Vec4 getReferenceCenter(RenderInfo ri)
@@ -30,5 +46,15 @@ public class RectangularTessellatorAccessible extends RectangularTessellator
 	public static DoubleBuffer getVertices(RenderInfo ri)
 	{
 		return ri.vertices;
+	}
+	
+	public static int getDensity(RenderInfo ri)
+	{
+		return ri.density;
+	}
+	
+	public static int getBufferIdVertices(RenderInfo ri)
+	{
+		return ri.bufferIdVertices;
 	}
 }
