433325b8f26a6fab953fabddb64d4add2c0eb89d
Michael de Hoog <michael.dehoog@ga.gov.au>
2010-11-24 06:25:26 +0000

    Added zoom smoothing to sub-surface orbit view if the eye is not close to the surface
    git-svn-id: svn://localhost/Software/Java/WorldWind@1419 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Common/src/au/gov/ga/worldwind/common/view/subsurface/SubSurfaceOrbitViewInputHandler.java
+++ Applications/Common/src/au/gov/ga/worldwind/common/view/subsurface/SubSurfaceOrbitViewInputHandler.java
@@ -14,22 +14,30 @@ public class SubSurfaceOrbitViewInputHandler extends OrbitViewInputHandler
 	protected void changeZoom(BasicOrbitView view, AnimationController animControl, double change,
 			ActionAttributes attrib)
 	{
+		Vec4 eyePoint = view.getCurrentEyePoint();
+		double altitude = SubSurfaceOrbitView.computeEyeAltitude(view.getDC(), view.getGlobe(), eyePoint);
+
+		//use the super method if far away from surface, as it does smoothing
+		if (altitude > 20000)
+		{
+			super.changeZoom(view, animControl, change, attrib);
+			return;
+		}
+
 		view.computeAndSetViewCenterIfNeeded();
 
 		if (animControl.get(VIEW_ANIM_ZOOM) != null)
 			animControl.remove(VIEW_ANIM_ZOOM);
 
-		Vec4 eyePoint = view.getCurrentEyePoint();
-		double altitude = SubSurfaceOrbitView.computeEyeAltitude(view.getDC(), view.getGlobe(), eyePoint);
-
 		view.setZoom(computeNewZoomFromAltitude(altitude, view.getZoom(), change, view.getOrbitViewLimits()));
+
 		view.firePropertyChange(AVKey.VIEW, null, view);
 	}
 
 	protected static double computeNewZoomFromAltitude(double altitude, double curZoom, double change,
 			OrbitViewLimits limits)
 	{
-		altitude = Math.max(20000, altitude);
+		altitude = Math.max(10000, altitude);
 		double newAltitude = computeNewZoom(altitude, change, limits);
 		double delta = newAltitude - altitude;
 		return curZoom + delta;
