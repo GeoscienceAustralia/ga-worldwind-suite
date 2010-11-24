fa2a32e94781b57ea3cc601fb7dcdf915cb0048d
Michael de Hoog <michael.dehoog@ga.gov.au>
2010-11-24 02:25:42 +0000

    Modified the StereoSkyGradientLayer sky calculation to use the view's computeHorizonDistance() instead of far clipping distance, so that it works for views that modify the default far clipping distance.
    git-svn-id: svn://localhost/Software/Java/WorldWind@1414 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Common/src/au/gov/ga/worldwind/common/layers/stereo/StereoSkyGradientLayer.java
+++ Applications/Common/src/au/gov/ga/worldwind/common/layers/stereo/StereoSkyGradientLayer.java
@@ -3,7 +3,9 @@ package au.gov.ga.worldwind.common.layers.stereo;
 import javax.media.opengl.GL;
 
 import au.gov.ga.worldwind.common.view.stereo.StereoView;
+import gov.nasa.worldwind.View;
 import gov.nasa.worldwind.geom.Matrix;
+import gov.nasa.worldwind.geom.Position;
 import gov.nasa.worldwind.layers.SkyGradientLayer;
 import gov.nasa.worldwind.render.DrawContext;
 
@@ -41,4 +43,61 @@ public class StereoSkyGradientLayer extends SkyGradientLayer
 			super.applyDrawProjection(dc);
 		}
 	}
+	
+	/* *******************************************************************************************************
+	 * The following methods are overridden to use computeHorizonDistance() in place of getFarClipDistance() *
+	 ******************************************************************************************************* */
+	
+	@Override
+	protected boolean isValid(DrawContext dc)
+    {
+        // Build or rebuild sky dome if horizon distance changed more then 100m
+        // Note: increasing this threshold may produce artefacts like far clipping at very low altitude
+        return this.glListId != -1 && Math.abs(this.lastRebuildHorizon - dc.getView().computeHorizonDistance()) <= 100;
+    }
+	
+	@Override
+	protected void updateSkyDone(DrawContext dc)
+    {
+        GL gl = dc.getGL();
+        View view = dc.getView();
+
+        if (this.glListId != -1)
+            gl.glDeleteLists(this.glListId, 1);
+
+        double tangentalDistance = view.computeHorizonDistance();
+        double distToCenterOfPlanet = view.getEyePoint().getLength3();
+        Position camPos = dc.getGlobe().computePositionFromPoint(view.getEyePoint());
+        double worldRadius = dc.getGlobe().getRadiusAt(camPos);
+        double camAlt = camPos.getElevation();
+
+        // horizon latitude degrees
+        double horizonLat = (-Math.PI / 2 + Math.acos(tangentalDistance / distToCenterOfPlanet))
+                * 180 / Math.PI;
+        // zenith latitude degrees
+        double zenithLat = 90;
+        float zenithOpacity = 1f;
+        float gradientBias = 2f;
+        if (camAlt >= thickness)
+        {
+            // Eye is above atmosphere
+            double tangentalDistanceZenith = Math.sqrt(distToCenterOfPlanet * distToCenterOfPlanet
+                    - (worldRadius + thickness) * (worldRadius + thickness));
+            zenithLat = (-Math.PI / 2 + Math.acos(tangentalDistanceZenith / distToCenterOfPlanet)) * 180 / Math.PI;
+            zenithOpacity = 0f;
+            gradientBias = 1f;
+        }
+        if (camAlt < thickness && camAlt > thickness * 0.7)
+        {
+            // Eye is entering atmosphere - outer 30%
+            double factor = (thickness - camAlt) / (thickness - thickness * 0.7);
+            zenithLat = factor * 90;
+            zenithOpacity = (float)factor;
+            gradientBias = 1f + (float)factor;
+        }
+
+        this.makeSkyDome(dc, (float) (tangentalDistance), horizonLat, zenithLat, SLICES, STACKS,
+            zenithOpacity, gradientBias);
+        this.lastRebuildHorizon = tangentalDistance;
+    }
 }
