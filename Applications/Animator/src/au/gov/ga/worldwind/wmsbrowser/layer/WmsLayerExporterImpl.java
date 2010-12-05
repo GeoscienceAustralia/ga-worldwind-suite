117b7f35b5c2212ecb161944b791c73588aa929f
James Navin <james.navin@ga.gov.au>
2010-12-05 23:47:12 +0000

    20101130 Adding support for exporting WMS layers as WW layer definitions (CWW-99)
    git-svn-id: svn://localhost/Software/Java/WorldWind@1477 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Animator/src/au/gov/ga/worldwind/wmsbrowser/layer/WmsLayerExporterImpl.java
+++ Applications/Animator/src/au/gov/ga/worldwind/wmsbrowser/layer/WmsLayerExporterImpl.java
@@ -1,25 +1,18 @@
 package au.gov.ga.worldwind.wmsbrowser.layer;
 
-import static au.gov.ga.worldwind.common.util.message.MessageSourceAccessor.*;
-import static au.gov.ga.worldwind.wmsbrowser.util.message.WmsBrowserMessageConstants.*;
-
-import gov.nasa.worldwind.avlist.AVKey;
 import gov.nasa.worldwind.avlist.AVList;
 import gov.nasa.worldwind.ogc.wms.WMSCapabilities;
 import gov.nasa.worldwind.util.Logging;
-import gov.nasa.worldwind.util.WWXML;
 import gov.nasa.worldwind.wms.WMSTiledImageLayer;
 import gov.nasa.worldwindow.core.WMSLayerInfo;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
-import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
 import au.gov.ga.worldwind.common.util.Validate;
 import au.gov.ga.worldwind.common.util.XMLUtil;
@@ -75,64 +68,9 @@ public class WmsLayerExporterImpl implements WmsLayerExporter
 		Validate.notNull(targetStream, "A target output stream is required");
 		Validate.notNull(layerInfo, "Layer information is required");
 		
-		AVList layerParameters = WMSTiledImageLayer.wmsGetParamsFromCapsDoc((WMSCapabilities)layerInfo.getCaps(), layerInfo.getParams());
-		
-		Document document = WWXML.createDocumentBuilder(false).newDocument();
-		
-		Element rootElement = addRootElement(document);
-		document.appendChild(rootElement);
-		
-		addGeneratedComment(rootElement);
-		addDisplayName(layerParameters, rootElement);
-		addService(layerParameters, rootElement);
-		addCacheName(layerParameters, rootElement);
-		addImageFormat(layerParameters, rootElement);
+		AVList wmsParameters = WMSTiledImageLayer.wmsGetParamsFromCapsDoc((WMSCapabilities)layerInfo.getCaps(), layerInfo.getParams());
+		Document document = WMSTiledImageLayer.createTiledImageLayerConfigDocument(wmsParameters);
 		
 		XMLUtil.saveDocumentToFormattedStream(document, targetStream);
-	}
-
-	private Element addRootElement(Document document)
-	{
-		Element rootElement = document.createElement("Layer");
-		WWXML.setIntegerAttribute(rootElement, "version", 1);
-		WWXML.setTextAttribute(rootElement, "layerType", "TiledImageLayer");
-		return rootElement;
-	}
-
-	private void addGeneratedComment(Element rootElement)
-	{
-		String comment = getMessage(getWmsExportedLayerCreationCommentKey(), new Date());
-		rootElement.appendChild(rootElement.getOwnerDocument().createComment(comment));
-	}
-	
-	private void addDisplayName(AVList layerParameters, Element rootElement)
-	{
-		Element displayNameElement = WWXML.appendElement(rootElement, "DisplayName");
-		WWXML.appendText(displayNameElement, null, layerParameters.getStringValue(AVKey.DISPLAY_NAME));
-	}
-	
-	private void addService(AVList layerParameters, Element rootElement)
-	{
-		Element serviceElement = WWXML.appendElement(rootElement, "Service");
-		WWXML.setTextAttribute(serviceElement, "serviceName", "OGC:WMS");
-		WWXML.setTextAttribute(serviceElement, "version", layerParameters.getStringValue(AVKey.WMS_VERSION));
-		
-		Element capabilitiesUrlElement = WWXML.appendElement(serviceElement, "GetCapabilitiesURL");
-		WWXML.appendText(capabilitiesUrlElement, null, layerParameters.getStringValue(AVKey.GET_CAPABILITIES_URL));
-		
-		Element mapUrlElement = WWXML.appendElement(serviceElement, "GetMapURL");
-		WWXML.appendText(mapUrlElement, null, layerParameters.getStringValue(AVKey.GET_MAP_URL));
-	}
-	
-	private void addCacheName(AVList layerParameters, Element rootElement)
-	{
-		Element cacheElement = WWXML.appendElement(rootElement, "DataCacheName");
-		WWXML.appendText(cacheElement, null, layerParameters.getStringValue(AVKey.DATA_CACHE_NAME));
-	}
-	
-	private void addImageFormat(AVList layerParameters, Element rootElement)
-	{
-		Element imageFormatElement = WWXML.appendElement(rootElement, "ImageFormat");
-		WWXML.appendText(imageFormatElement, null, layerParameters.getStringValue(AVKey.IMAGE_FORMAT));
 	}
 }
