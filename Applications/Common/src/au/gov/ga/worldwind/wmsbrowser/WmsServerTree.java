cf3a9be80bd4ebd382d2653314057ca91ec90585
James Navin <james.navin@ga.gov.au>
2011-05-24 02:14:29 +0000

    20110524 Adding more appropriate icons to the WMS server tree (CORPWWS-156)
    git-svn-id: svn://localhost/Software/Java/WorldWind@1744 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Common/src/au/gov/ga/worldwind/wmsbrowser/WmsServerTree.java
+++ Applications/Common/src/au/gov/ga/worldwind/wmsbrowser/WmsServerTree.java
@@ -2,12 +2,20 @@ package au.gov.ga.worldwind.wmsbrowser;
 
 import gov.nasa.worldwindow.core.WMSLayerInfo;
 
+import java.awt.Component;
 import java.awt.Point;
 
+import javax.swing.ImageIcon;
+import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.TreeCellRenderer;
 
+import au.gov.ga.worldwind.common.ui.lazytree.ErrorNode;
 import au.gov.ga.worldwind.common.ui.lazytree.LazyTree;
 import au.gov.ga.worldwind.common.ui.lazytree.LazyTreeModel;
+import au.gov.ga.worldwind.common.ui.lazytree.LoadingNode;
+import au.gov.ga.worldwind.common.util.Icons;
 
 /**
  * An extension of the {@link LazyTree} class customised for
@@ -24,6 +32,8 @@ class WmsServerTree extends LazyTree
 		setDragEnabled(false);
 		setEditable(false);
 		setShowsRootHandles(true);
+		
+		setCellRenderer(new WmsServerTreeCellRenderer());
 	}
 
 	@Override
@@ -54,5 +64,49 @@ class WmsServerTree extends LazyTree
 	public void selectTreeElementAtPoint(Point p)
 	{
 		getSelectionModel().setSelectionPath(getPathForLocation((int)p.getX(), (int)p.getY()));
+	}
+	
+	/**
+	 * A {@link TreeCellRenderer} that decorates the WMS tree with icons appropriate for servers/layers etc.
+	 */
+	private static class WmsServerTreeCellRenderer extends DefaultTreeCellRenderer
+	{
+		private ImageIcon loadingIcon;
+		
+		@Override
+		public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus)
+		{
+			if (loadingIcon == null)
+			{
+				loadingIcon = Icons.newLoadingIcon();
+				loadingIcon.setImageObserver(tree);
+			}
+			
+			super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
+			
+			if (value instanceof ErrorNode)
+			{
+				setIcon(Icons.error.getIcon());
+			}
+			else if (value instanceof LoadingNode)
+			{
+				setIcon(loadingIcon);
+			}
+			else if (value instanceof DefaultMutableTreeNode)
+			{
+				Object nodeObject = ((DefaultMutableTreeNode)value).getUserObject();
+				
+				if (nodeObject instanceof WmsServerTreeObject)
+				{
+					setIcon(Icons.wmsbrowser.getIcon());
+				}
+				if (nodeObject instanceof WMSLayerInfo)
+				{
+					setIcon(Icons.image.getIcon());
+				}
+			}
+			
+			return this;
+		}
 	}
 }
