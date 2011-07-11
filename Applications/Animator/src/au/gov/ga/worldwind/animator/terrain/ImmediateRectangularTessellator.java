04c36130398c8ffaf178787d2eeecbd53f143c1e
James Navin <james.navin@ga.gov.au>
2011-07-11 00:22:22 +0000

    20110711 Refactoring WireframeRectangularTessellator to common so it can be used in the Animator
    git-svn-id: svn://localhost/Software/Java/WorldWind@1797 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Animator/src/au/gov/ga/worldwind/animator/terrain/ImmediateRectangularTessellator.java
+++ Applications/Animator/src/au/gov/ga/worldwind/animator/terrain/ImmediateRectangularTessellator.java
@@ -2,14 +2,14 @@ package au.gov.ga.worldwind.animator.terrain;
 
 import gov.nasa.worldwind.render.DrawContext;
 import gov.nasa.worldwind.terrain.RectangularTessellator;
-import gov.nasa.worldwind.terrain.RectangularTessellatorAccessible;
+import au.gov.ga.worldwind.common.terrain.WireframeRectangularTessellator;
 
 /**
  * An extension of the {@link RectangularTessellator} that ignores the cache and
- * regenerates vertices every repaint, instead of cacheing and regenerating
+ * regenerates vertices every repaint, instead of caching and regenerating
  * every second.
  */
-public class ImmediateRectangularTessellator extends RectangularTessellatorAccessible
+public class ImmediateRectangularTessellator extends WireframeRectangularTessellator
 {
 	@Override
 	protected void makeVerts(DrawContext dc, RectTile tile)
