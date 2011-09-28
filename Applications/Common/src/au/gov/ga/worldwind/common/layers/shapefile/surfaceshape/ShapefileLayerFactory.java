eb5ac16c77d7299ef8a27db1a529f7ad0b1a63c9
James Navin <james.navin@ga.gov.au>
2011-09-28 04:47:49 +0000

    20110928 Updating to worldwind-71.66 daily build (CORPWWS-222)
    git-svn-id: svn://localhost/Software/Java/WorldWind@1852 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Common/src/au/gov/ga/worldwind/common/layers/shapefile/surfaceshape/ShapefileLayerFactory.java
+++ Applications/Common/src/au/gov/ga/worldwind/common/layers/shapefile/surfaceshape/ShapefileLayerFactory.java
@@ -3,7 +3,6 @@ package au.gov.ga.worldwind.common.layers.shapefile.surfaceshape;
 import gov.nasa.worldwind.avlist.AVKey;
 import gov.nasa.worldwind.avlist.AVList;
 import gov.nasa.worldwind.avlist.AVListImpl;
-import gov.nasa.worldwind.examples.util.ShapefileLoader;
 import gov.nasa.worldwind.formats.shapefile.Shapefile;
 import gov.nasa.worldwind.geom.Sector;
 import gov.nasa.worldwind.layers.AbstractLayer;
@@ -16,6 +15,7 @@ import gov.nasa.worldwind.render.ShapeAttributes;
 import gov.nasa.worldwind.render.SurfaceShape;
 import gov.nasa.worldwind.util.Logging;
 import gov.nasa.worldwind.util.WWXML;
+import gov.nasa.worldwindx.examples.util.ShapefileLoader;
 
 import java.awt.Color;
 import java.net.MalformedURLException;
@@ -30,8 +30,6 @@ import au.gov.ga.worldwind.common.util.AVKeyMore;
 /**
  * Layer factory which creates a {@link Layer} from an AVList or XML element.
  * The parameters must contain a URL which points to a ESRI shapefile.
- * 
- * @author Michael de Hoog
  */
 public class ShapefileLayerFactory
 {
@@ -78,11 +76,15 @@ public class ShapefileLayerFactory
 
 		s = params.getStringValue(AVKey.DISPLAY_NAME);
 		if (s != null)
+		{
 			layer.setName(s);
+		}
 
 		Long l = (Long) params.getValue(AVKey.EXPIRY_TIME);
 		if (l != null)
+		{
 			layer.setExpiryTime(l);
+		}
 
 		//set the shape drawing attributes
 		ShapeAttributes attributes = new BasicShapeAttributes();
@@ -95,7 +97,9 @@ public class ShapefileLayerFactory
 			for (Renderable renderable : rl.getRenderables())
 			{
 				if (renderable instanceof SurfaceShape)
+				{
 					((SurfaceShape) renderable).setAttributes(attributes);
+				}
 			}
 		}
 
@@ -117,7 +121,9 @@ public class ShapefileLayerFactory
 	public static Layer createLayer(Element domElement, AVList params)
 	{
 		if (params == null)
+		{
 			params = new AVListImpl();
+		}
 
 		XPath xpath = WWXML.makeXPath();
 
@@ -142,43 +148,63 @@ public class ShapefileLayerFactory
 	{
 		Boolean b = (Boolean) params.getValue(ShapeAttributeKey.DRAW_INTERIOR);
 		if (b != null)
+		{
 			attributes.setDrawInterior(b);
+		}
 
 		b = (Boolean) params.getValue(ShapeAttributeKey.DRAW_OUTLINE);
 		if (b != null)
+		{
 			attributes.setDrawOutline(b);
+		}
 
 		b = (Boolean) params.getValue(ShapeAttributeKey.ANTIALIASING);
 		if (b != null)
+		{
 			attributes.setEnableAntialiasing(b);
+		}
 
 		Color c = (Color) params.getValue(ShapeAttributeKey.INTERIOR_COLOR);
 		if (c != null)
+		{
 			attributes.setInteriorMaterial(new Material(c));
+		}
 
 		c = (Color) params.getValue(ShapeAttributeKey.OUTLINE_COLOR);
 		if (c != null)
+		{
 			attributes.setOutlineMaterial(new Material(c));
+		}
 
 		Double d = (Double) params.getValue(ShapeAttributeKey.INTERIOR_OPACITY);
 		if (d != null)
+		{
 			attributes.setInteriorOpacity(d);
+		}
 
 		d = (Double) params.getValue(ShapeAttributeKey.OUTLINE_OPACITY);
 		if (d != null)
+		{
 			attributes.setOutlineOpacity(d);
+		}
 
 		d = (Double) params.getValue(ShapeAttributeKey.OUTLINE_WIDTH);
 		if (d != null)
+		{
 			attributes.setOutlineWidth(d);
+		}
 
 		Integer i = (Integer) params.getValue(ShapeAttributeKey.STIPPLE_FACTOR);
 		if (i != null)
+		{
 			attributes.setOutlineStippleFactor(i);
+		}
 
 		i = (Integer) params.getValue(ShapeAttributeKey.STIPPLE_PATTERN);
 		if (i != null)
+		{
 			attributes.setOutlineStipplePattern(i.shortValue());
+		}
 	}
 
 	/**
@@ -194,30 +220,22 @@ public class ShapefileLayerFactory
 	protected static AVList getAttributeParams(Element domElement, AVList params)
 	{
 		if (params == null)
+		{
 			params = new AVListImpl();
+		}
 
 		XPath xpath = WWXML.makeXPath();
 
-		WWXML.checkAndSetBooleanParam(domElement, params, ShapeAttributeKey.DRAW_INTERIOR,
-				"DrawInterior", xpath);
-		WWXML.checkAndSetBooleanParam(domElement, params, ShapeAttributeKey.DRAW_OUTLINE,
-				"DrawOutline", xpath);
-		WWXML.checkAndSetBooleanParam(domElement, params, ShapeAttributeKey.ANTIALIASING,
-				"Antialiasing", xpath);
-		WWXML.checkAndSetColorParam(domElement, params, ShapeAttributeKey.INTERIOR_COLOR,
-				"InteriorColor", xpath);
-		WWXML.checkAndSetColorParam(domElement, params, ShapeAttributeKey.OUTLINE_COLOR,
-				"OutlineColor", xpath);
-		WWXML.checkAndSetDoubleParam(domElement, params, ShapeAttributeKey.INTERIOR_OPACITY,
-				"InteriorOpacity", xpath);
-		WWXML.checkAndSetDoubleParam(domElement, params, ShapeAttributeKey.OUTLINE_OPACITY,
-				"OutlineOpacity", xpath);
-		WWXML.checkAndSetDoubleParam(domElement, params, ShapeAttributeKey.OUTLINE_WIDTH,
-				"OutlineWidth", xpath);
-		WWXML.checkAndSetIntegerParam(domElement, params, ShapeAttributeKey.STIPPLE_FACTOR,
-				"StippleFactor", xpath);
-		WWXML.checkAndSetIntegerParam(domElement, params, ShapeAttributeKey.STIPPLE_PATTERN,
-				"StipplePattern", xpath);
+		WWXML.checkAndSetBooleanParam(domElement, params, ShapeAttributeKey.DRAW_INTERIOR, "DrawInterior", xpath);
+		WWXML.checkAndSetBooleanParam(domElement, params, ShapeAttributeKey.DRAW_OUTLINE, "DrawOutline", xpath);
+		WWXML.checkAndSetBooleanParam(domElement, params, ShapeAttributeKey.ANTIALIASING, "Antialiasing", xpath);
+		WWXML.checkAndSetColorParam(domElement, params, ShapeAttributeKey.INTERIOR_COLOR, "InteriorColor", xpath);
+		WWXML.checkAndSetColorParam(domElement, params, ShapeAttributeKey.OUTLINE_COLOR, "OutlineColor", xpath);
+		WWXML.checkAndSetDoubleParam(domElement, params, ShapeAttributeKey.INTERIOR_OPACITY, "InteriorOpacity", xpath);
+		WWXML.checkAndSetDoubleParam(domElement, params, ShapeAttributeKey.OUTLINE_OPACITY, "OutlineOpacity", xpath);
+		WWXML.checkAndSetDoubleParam(domElement, params, ShapeAttributeKey.OUTLINE_WIDTH, "OutlineWidth", xpath);
+		WWXML.checkAndSetIntegerParam(domElement, params, ShapeAttributeKey.STIPPLE_FACTOR, "StippleFactor", xpath);
+		WWXML.checkAndSetIntegerParam(domElement, params, ShapeAttributeKey.STIPPLE_PATTERN, "StipplePattern", xpath);
 
 		return params;
 	}
