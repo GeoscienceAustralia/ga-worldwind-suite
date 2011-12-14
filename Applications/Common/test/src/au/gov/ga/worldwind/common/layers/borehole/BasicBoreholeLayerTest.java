e98d2e5f9f4989344affb121fcca3d43afa4e70b
Michael de Hoog <michael.dehoog@ga.gov.au>
2011-12-14 05:00:32 +0000

    Added Borehole layer creation unit test
    git-svn-id: svn://localhost/Software/Java/WorldWind@2056 4cd5884b-1955-4949-bbec-e4dd67779ed6

---
+++ Applications/Common/test/src/au/gov/ga/worldwind/common/layers/borehole/BasicBoreholeLayerTest.java
@@ -0,0 +1,62 @@
+package au.gov.ga.worldwind.common.layers.borehole;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import gov.nasa.worldwind.avlist.AVList;
+import gov.nasa.worldwind.avlist.AVListImpl;
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.util.WWXML;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+import au.gov.ga.worldwind.common.layers.styled.Attribute;
+import au.gov.ga.worldwind.common.layers.styled.StringWithPlaceholderGetter;
+import au.gov.ga.worldwind.common.util.AVKeyMore;
+
+/**
+ * Unit tests for the {@link BasicBoreholeLayer} class
+ */
+public class BasicBoreholeLayerTest
+{
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testCreateBoreholeLayer()
+	{
+		Document doc = WWXML.openDocument(getClass().getResourceAsStream("boreholeLayer.xml"));
+		BoreholeLayer boreholeLayer =
+				BoreholeLayerFactory.createBoreholeLayer(doc.getDocumentElement(), new AVListImpl());
+
+		AVList params = (AVList) boreholeLayer.getValue(AVKeyMore.CONSTRUCTION_PARAMETERS);
+		assertNotNull(params);
+
+		// Attributes should not be empty
+		List<Attribute> attributes = (List<Attribute>) params.getValue(AVKeyMore.BOREHOLE_ATTRIBUTES);
+		assertNotNull(attributes);
+		assertTrue(attributes.size() == 1);
+
+		// Attributes should not be empty
+		attributes = (List<Attribute>) params.getValue(AVKeyMore.BOREHOLE_SAMPLE_ATTRIBUTES);
+		assertNotNull(attributes);
+		assertTrue(attributes.size() == 2);
+
+		assertEquals("NAME", attributes.get(1).getName());
+		assertEquals("Name: %v%", StringWithPlaceholderGetter.getTextString(attributes.get(1)));
+		assertEquals("%v%", StringWithPlaceholderGetter.getTextPlaceholder(attributes.get(1)));
+
+		// Test regular parameters
+		assertEquals("Borehole Layer", boreholeLayer.getName());
+		assertEquals("GA/Boreholes.zip", boreholeLayer.getDataCacheName());
+		
+		// Test adding a borehole sample
+		AVList attributeValues = new AVListImpl();
+		attributeValues.setValue("HOLE_ID", "id1");
+		attributeValues.setValue("FROM", 50.0);
+		attributeValues.setValue("TO", 60.0);
+		boreholeLayer.addBoreholeSample(Position.ZERO, attributeValues);
+		boreholeLayer.loadComplete();
+	}
+}
