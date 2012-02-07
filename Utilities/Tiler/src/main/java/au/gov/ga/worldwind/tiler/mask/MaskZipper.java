8e329b7ae371b38cb28bf3e75a97ee1947db3f84
Michael de Hoog <michael.dehoog@ga.gov.au>
2012-02-07 22:53:50 +0000

    Added some code for creating multiple layer definitions for multiple zip files (dodgy hard-coded network paths)
    git-svn-id: svn://localhost/Software/Java/WorldWind@2154 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Utilities/Tiler/src/main/java/au/gov/ga/worldwind/tiler/mask/MaskZipper.java
+++ Utilities/Tiler/src/main/java/au/gov/ga/worldwind/tiler/mask/MaskZipper.java
@@ -1,29 +1,121 @@
 package au.gov.ga.worldwind.tiler.mask;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MaskZipper
 {
 	public static void main(String[] args)
 	{
-		File imageDir = new File("D:/Groundwater Data/layers/tiles/bh_spot2p5m_wgs84_ecw_tiles");
+		/*File imageDir = new File("D:/Groundwater Data/layers/tiles/bh_spot2p5m_wgs84_ecw_tiles");
 		File maskDir = new File("D:/Groundwater Data/layers/tiles/bh_spot2p5m_wgs84_ecw_mask");
 		File outputDir = new File("D:/Groundwater Data/layers/tiles/bh_spot2p5m_wgs84_ecw");
 		String imageExt = "jpg";
 		String maskExt = "png";
 		String outputExt = "zip";
-		zip(imageDir, imageExt, maskDir, maskExt, outputDir, outputExt);
+		zip(imageDir, imageExt, maskDir, maskExt, outputDir, outputExt);*/
+
+		/*File dir =
+				new File(
+						"V:/projects/presentations/11-5902 - Broken Hill 3D model data visualisation/Source/AEM/elevation_tif");
+		File[] files = dir.listFiles();
+		String tilesSuffix = "_tiles";
+		String maskSuffix = "_mask";
+		String imageExt = "jpg";
+		String maskExt = "png";
+		String outputExt = "zip";
+		for (File file : files)
+		{
+			if (file.isDirectory() && file.getName().endsWith(tilesSuffix))
+			{
+				File tilesDir = file;
+				File outputDir =
+						new File(file.getAbsolutePath().substring(0,
+								file.getAbsolutePath().length() - tilesSuffix.length()));
+				File maskDir = new File(outputDir.getAbsolutePath() + maskSuffix);
+				if (maskDir.exists())
+				{
+					zip(tilesDir, imageExt, maskDir, maskExt, outputDir, outputExt);
+					for (File maskFile : maskDir.listFiles())
+					{
+						if (maskFile.getName().endsWith(".log"))
+						{
+							copyFile(maskFile, new File(outputDir, maskFile.getName()));
+						}
+					}
+					for (File tileFile : tilesDir.listFiles())
+					{
+						if (tileFile.getName().endsWith(".log"))
+						{
+							copyFile(tileFile, new File(outputDir, tileFile.getName()));
+						}
+					}
+				}
+			}
+		}*/
+
+		File dir =
+				new File(
+						"V:/projects/presentations/11-5902 - Broken Hill 3D model data visualisation/Source/AEM/elevation_tiled");
+		File template = new File(dir, "e01_m050_m045.xml");
+		Pattern elevationPattern = Pattern.compile(".+_([mp])(\\d\\d\\d)_.+");
+		File[] files = dir.listFiles();
+		for (File file : files)
+		{
+			if (file.getName().endsWith(".zip"))
+			{
+				String fileWithoutExtension = file.getName().substring(0, file.getName().length() - 4);
+				File output = new File(dir, fileWithoutExtension + ".xml");
+				if (!output.exists())
+				{
+					Matcher matcher = elevationPattern.matcher(file.getName());
+					if (matcher.matches())
+					{
+						boolean negative = matcher.group(1).equals("m");
+						int elevation = Integer.parseInt(matcher.group(2)) * (negative ? -1 : 1);
+						try
+						{
+							BufferedWriter writer = new BufferedWriter(new FileWriter(output));
+							BufferedReader reader = new BufferedReader(new FileReader(template));
+							String line;
+							while ((line = reader.readLine()) != null)
+							{
+								line = line.replaceAll("e01_m050_m045", fileWithoutExtension);
+								line =
+										line.replaceAll("ElevationOffset\\(-50\\)", "ElevationOffset(" + elevation
+												+ ")");
+								writer.append(line + "\r\n");
+							}
+							writer.close();
+							reader.close();
+							
+						}
+						catch (Exception e)
+						{
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
 	}
 
-	public static void zip(File imageDir, String imageExt, File maskDir, String maskExt,
-			File outputDir, String outputExt)
+	public static void zip(File imageDir, String imageExt, File maskDir, String maskExt, File outputDir,
+			String outputExt)
 	{
 		try
 		{
@@ -31,8 +123,7 @@ public class MaskZipper
 			List<File> masks = new ArrayList<File>();
 			List<File> outputs = new ArrayList<File>();
 
-			addImages(imageDir, images, masks, outputs, imageDir, imageExt, maskDir, maskExt,
-					outputDir, outputExt);
+			addImages(imageDir, images, masks, outputs, imageDir, imageExt, maskDir, maskExt, outputDir, outputExt);
 
 			System.out.println("Found " + images.size() + " images");
 
@@ -45,24 +136,15 @@ public class MaskZipper
 				int indexOfLastPeriod = imageFile.getName().lastIndexOf('.');
 				String filename = imageFile.getName().substring(0, indexOfLastPeriod);
 
-				imageFile =
-						copyfile(imageFile, new File(filename + "."
-								+ (maskFile == null ? maskExt : imageExt)));
-				maskFile =
-						maskFile == null ? null : copyfile(maskFile, new File(filename + "_mask."
-								+ maskExt));
+				imageFile = copyfile(imageFile, new File(filename + "." + (maskFile == null ? maskExt : imageExt)));
+				maskFile = maskFile == null ? null : copyfile(maskFile, new File(filename + "_mask." + maskExt));
 
-				System.out.println("Writing " + outputFile + " (" + (i + 1) + "/" + images.size()
-						+ " - " + ((i + 1) * 100 / images.size()) + "%)");
+				System.out.println("Writing " + outputFile + " (" + (i + 1) + "/" + images.size() + " - "
+						+ ((i + 1) * 100 / images.size()) + "%)");
 
 				String command =
-						"7za a -tzip \""
-								+ outputFile.getAbsolutePath()
-								+ "\" \""
-								+ imageFile.getAbsolutePath()
-								+ "\""
-								+ (maskFile == null ? "" : " \"" + maskFile.getAbsolutePath()
-										+ "\"");
+						"7za a -tzip \"" + outputFile.getAbsolutePath() + "\" \"" + imageFile.getAbsolutePath() + "\""
+								+ (maskFile == null ? "" : " \"" + maskFile.getAbsolutePath() + "\"");
 				Process process = Runtime.getRuntime().exec(command);
 				process.waitFor();
 
@@ -95,9 +177,8 @@ public class MaskZipper
 		return dst;
 	}
 
-	protected static void addImages(File dir, List<File> images, List<File> masks,
-			List<File> outputs, File imageDir, String imageExt, File maskDir, String maskExt,
-			File outputDir, String outputExt)
+	protected static void addImages(File dir, List<File> images, List<File> masks, List<File> outputs, File imageDir,
+			String imageExt, File maskDir, String maskExt, File outputDir, String outputExt)
 	{
 		File[] files = dir.listFiles();
 		if (files != null)
@@ -106,8 +187,7 @@ public class MaskZipper
 			{
 				if (file.isDirectory())
 				{
-					addImages(file, images, masks, outputs, imageDir, imageExt, maskDir, maskExt,
-							outputDir, outputExt);
+					addImages(file, images, masks, outputs, imageDir, imageExt, maskDir, maskExt, outputDir, outputExt);
 				}
 				else if (file.getName().toLowerCase().endsWith("." + imageExt))
 				{
@@ -149,6 +229,42 @@ public class MaskZipper
 					}
 				}
 			}
+		}
+	}
+
+	public static void copyFile(File sourceFile, File destFile)
+	{
+		try
+		{
+			if (!destFile.exists())
+			{
+				destFile.createNewFile();
+			}
+
+			FileChannel source = null;
+			FileChannel destination = null;
+
+			try
+			{
+				source = new FileInputStream(sourceFile).getChannel();
+				destination = new FileOutputStream(destFile).getChannel();
+				destination.transferFrom(source, 0, source.size());
+			}
+			finally
+			{
+				if (source != null)
+				{
+					source.close();
+				}
+				if (destination != null)
+				{
+					destination.close();
+				}
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
 		}
 	}
 }
