94763a355ea6ebb9eb038ee7820d475486ee72ce
Michael de Hoog <michael.dehoog@ga.gov.au>
2012-02-28 01:44:17 +0000

    Created the Deployer project, which contains an Ant build script that can be used to deploy the Viewer, Animator, and Tiler to network locations on the GA network.
    git-svn-id: svn://localhost/Software/Java/WorldWind@2232 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Viewer/run.bat
+++
@@ -1,1 +0,0 @@
-java -Xmx1024m -cp bin;lib/worldwind.jar;lib/jogl.jar;lib/gluegen-rt.jar -Djava.library.path=lib/jogl/windows-i586 -Dsun.java2d.noddraw=true au.gov.ga.worldwind.application.Application
