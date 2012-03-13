45e6a55c384c83388191fb163e8d6a0ef37940a9
Michael de Hoog <michael.dehoog@ga.gov.au>
2012-03-13 03:33:22 +0000

    Fixed the TGAScreenshot class to use less memory (no longer using a mapped file, but an OutputStream instead), and to read alpha screenshots using BGRA instead of ABGR_EXT
    git-svn-id: svn://localhost/Software/Java/WorldWind@2283 4cd5884b-1955-4949-bbec-e4dd67779ed6

--- Applications/Animator/src/main/java/au/gov/ga/worldwind/animator/util/TGAScreenshot.java
+++ Applications/Animator/src/main/java/au/gov/ga/worldwind/animator/util/TGAScreenshot.java
@@ -16,11 +16,12 @@
 package au.gov.ga.worldwind.animator.util;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
+import java.nio.channels.Channels;
+import java.nio.channels.WritableByteChannel;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GLException;
@@ -36,15 +37,6 @@ import com.sun.opengl.util.Screenshot;
  */
 public class TGAScreenshot
 {
-	private static void checkExtABGR()
-	{
-		GL gl = GLU.getCurrentGL();
-		if (!gl.isExtensionAvailable("GL_EXT_abgr"))
-		{
-			throw new IllegalArgumentException("Saving alpha channel requires GL_EXT_abgr");
-		}
-	}
-
 	public static void writeToTargaFile(File file, int width, int height) throws GLException, IOException
 	{
 		writeToTargaFile(file, width, height, false);
@@ -65,11 +57,9 @@ public class TGAScreenshot
 			return;
 		}
 
-		checkExtABGR();
-
 		TGAWriter writer = new TGAWriter();
 		writer.open(file, width, height, alpha);
-		ByteBuffer bgr = writer.getImageData();
+		ByteBuffer buf = writer.getImageData();
 
 		GL gl = GLU.getCurrentGL();
 
@@ -77,10 +67,10 @@ public class TGAScreenshot
 		PixelStorageModes psm = new PixelStorageModes();
 		psm.save(gl);
 
-		int readbackType = (alpha ? GL.GL_ABGR_EXT : GL.GL_BGR);
+		int readbackType = (alpha ? GL.GL_BGRA : GL.GL_BGR);
 
 		// read the BGR values into the image buffer
-		gl.glReadPixels(x, y, width, height, readbackType, GL.GL_UNSIGNED_BYTE, bgr);
+		gl.glReadPixels(x, y, width, height, readbackType, GL.GL_UNSIGNED_BYTE, buf);
 
 		// Restore pixel storage modes
 		psm.restore(gl);
@@ -93,10 +83,8 @@ public class TGAScreenshot
 	{
 		private static final int TARGA_HEADER_SIZE = 18;
 
-		private FileChannel ch;
-		private MappedByteBuffer image;
+		private OutputStream os;
 		private ByteBuffer buf;
-		private boolean alpha;
 
 		public TGAWriter()
 		{
@@ -104,33 +92,22 @@ public class TGAScreenshot
 
 		public void open(File file, int width, int height, boolean alpha) throws IOException
 		{
-			this.alpha = alpha;
-			RandomAccessFile out = new RandomAccessFile(file, "rw");
-			ch = out.getChannel();
+			os = new FileOutputStream(file);
 			int pixelSize = (alpha ? 32 : 24);
 			int numChannels = (alpha ? 4 : 3);
 
-			int fileLength = TARGA_HEADER_SIZE + width * height * numChannels;
-			out.setLength(fileLength);
-			image = ch.map(FileChannel.MapMode.READ_WRITE, 0, fileLength);
-
 			// write the TARGA header
-			image.put(0, (byte) 0).put(1, (byte) 0);
-			image.put(2, (byte) 2); // uncompressed type
-			image.put(12, (byte) (width & 0xFF)); // width
-			image.put(13, (byte) (width >> 8)); // width
-			image.put(14, (byte) (height & 0xFF)); // height
-			image.put(15, (byte) (height >> 8)); // height
-			image.put(16, (byte) pixelSize); // pixel size
+			byte[] header = new byte[TARGA_HEADER_SIZE];
+			header[2] = 2;
+			header[12] = (byte) (width & 0xFF);
+			header[13] = (byte) (width >> 8);
+			header[14] = (byte) (height & 0xFF);
+			header[15] = (byte) (height >> 8);
+			header[16] = (byte) pixelSize;
+			os.write(header);
 
-			// go to image data position
-			image.position(TARGA_HEADER_SIZE);
-			if (alpha)
-				//create buffer for jogl to save pixels into
-				buf = BufferUtil.newByteBuffer(width * height * numChannels);
-			else
-				// jogl needs a sliced buffer
-				buf = image.slice();
+			//create buffer for jogl to save pixels into
+			buf = BufferUtil.newByteBuffer(width * height * numChannels);
 		}
 
 		public ByteBuffer getImageData()
@@ -140,21 +117,11 @@ public class TGAScreenshot
 
 		public void close() throws IOException
 		{
-			if (alpha)
-			{
-				buf.rewind();
-				while (buf.hasRemaining())
-				{
-					image.put(image.position() + 3, buf.get());
-					image.put(buf.get());
-					image.put(buf.get());
-					image.put(buf.get());
-					image.get();
-				}
-			}
-
-			// close the file channel
-			ch.close();
+			WritableByteChannel channel = Channels.newChannel(os);
+			buf.rewind();
+			channel.write(buf);
+			channel.close();
+			os.close();
 			buf = null;
 		}
 	}
