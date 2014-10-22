f487eb9be252c6313a95507e40c14759389dd681
Michael de Hoog <michael.dehoog@ga.gov.au>
2014-10-22 12:27:47 +1100

    Updated tester

--- Tiler/src/main/java/au/gov/ga/worldwind/tiler/shapefile/Tester.java
+++ Tiler/src/main/java/au/gov/ga/worldwind/tiler/shapefile/Tester.java
@@ -25,16 +25,18 @@ public class Tester
 {
 	public static void main(String[] args)
 	{
-		File input = new File("C:/WINNT/Profiles/u97852/Desktop/calcrete/calcrete.shp");
+		//File input = new File("C:/WINNT/Profiles/u97852/Desktop/calcrete/calcrete.shp");
 		/*File input0 = new File("C:/WINNT/Profiles/u97852/Desktop/GSHHS_shp/c/GSHHS_c_L1.shp");
 		File input1 = new File("C:/WINNT/Profiles/u97852/Desktop/GSHHS_shp/l/GSHHS_l_L1.shp");
 		File input2 = new File("C:/WINNT/Profiles/u97852/Desktop/GSHHS_shp/i/GSHHS_i_L1.shp");
 		File input3 = new File("C:/WINNT/Profiles/u97852/Desktop/GSHHS_shp/h/GSHHS_h_L1.shp");
 		File input4 = new File("C:/WINNT/Profiles/u97852/Desktop/GSHHS_shp/f/GSHHS_f_L1.shp");*/
+		File input = new File("E:/World/water/worldwaterbodies/worldwaterbodies_noaustralia.shp");
+		//File input = new File("E:/World/water/worldwaterbodies/polygonthatbreakstiler.shp");
 
-		File output = new File("C:/WINNT/Profiles/u97852/Desktop/calcrete/tiled");
-		double lzts = 36;
-		int level = 0;
+		File output = new File("E:/World/water/worldwaterbodies_noaustralia_shp");
+		double lzts = 180;
+		int level = 9;
 		LatLon origin = LatLon.DEFAULT_ORIGIN;
 		//File input = input1;
 
